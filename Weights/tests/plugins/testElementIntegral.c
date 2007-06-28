c0b8c6b2e554938abbb61f924da1db0a3be3a491
PatrickSunter <devnull@localhost>
2007-06-28 05:59:08 +0000

    Changed the standard names testElementIntegral.c looks for swarms under to be more sensible.

--- Weights/tests/plugins/testElementIntegral.c
+++ Weights/tests/plugins/testElementIntegral.c
@@ -38,7 +38,7 @@
 **  License along with this library; if not, write to the Free Software
 **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** $Id: testElementIntegral.c 476 2007-06-27 00:32:30Z DavidLee $
+** $Id: testElementIntegral.c 485 2007-06-28 05:59:08Z PatrickSunter $
 **
 **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -146,9 +146,9 @@ void testElementIntegral_CompareAgainstReferenceSolution(DiscretisationContext* 
 
 
 void PICellerator_testElementIntegral( DiscretisationContext* context ) {
-	Swarm*              integrationSwarm = (Swarm*)LiveComponentRegister_Get( context->CF->LCRegister, "swarm" );
-	Swarm*              materialSwarm    = (Swarm*)LiveComponentRegister_Get( context->CF->LCRegister, "picIntegrationPoints" );
-	FeMesh* mesh             = (FeMesh*) LiveComponentRegister_Get( context->CF->LCRegister, "mesh-linear" );
+	Swarm*              integrationSwarm = (Swarm*)LiveComponentRegister_Get( context->CF->LCRegister, "integrationSwarm" );
+	Swarm*              materialSwarm    = (Swarm*)LiveComponentRegister_Get( context->CF->LCRegister, "materialPoints" );
+	FeMesh* mesh             = (FeMesh*) LiveComponentRegister_Get( context->CF->LCRegister, "linearMesh" );
 	WeightsCalculator*  weights          = (WeightsCalculator*) LiveComponentRegister_Get( context->CF->LCRegister, "weights" );
 	FeVariable*         feVariable;
 	Element_LocalIndex  lElement_I       = 0;
