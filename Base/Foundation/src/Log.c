1a0e577f8e046e24648126d00b7bf121d3165409
LouisMoresi <devnull@localhost>
2007-11-13 03:18:58 +0000

    Added a check for a previous definition of the log file name and a fallback to stderr if the log file cannot be created.
    L.
    
    I guess it would be useful to make the log file name / path configurable at run time ... one day

--- Base/Foundation/src/Log.c
+++ Base/Foundation/src/Log.c
@@ -29,7 +29,9 @@
 #include <stdarg.h>
 #include <mpi.h>
 
+#ifndef STGERMAIN_LOGFILE_NAME
 #define STGERMAIN_LOGFILE_NAME "stgermain.log"
+#endif 
 
 /* empty out the file */
 void _init_stg_log_printf( const char *name )
@@ -43,22 +45,29 @@ void _init_stg_log_printf( const char *name )
         sprintf( err, name );
         fp = fopen( err, "w" );
 
+		/* It's useful to have a safe fallback here */ 
+		
+		if(fp==NULL) {
+			fp = stderr;
+		}	
+
         /* write header for log file */
         MPI_Comm_rank( MPI_COMM_WORLD, &rank );
         MPI_Get_processor_name( mname, &length );
 
         fprintf( fp, "=== StGermain Parallel log ===\n" );
         fprintf( fp, "=== Generated on %s, by rank %d ===\n", mname, rank );
-
-        fclose( fp );
+		
+		if(fp != stderr)
+        	fclose( fp );
 }
 
 void _stg_log_write_filename( char name[] )
 {
 	int rank;
-
+	
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-        sprintf( name, "%s.p%d", STGERMAIN_LOGFILE_NAME, rank );
+    sprintf( name, "%s.p%d" , STGERMAIN_LOGFILE_NAME, rank ); 
 }
 
 
@@ -77,13 +86,18 @@ void stg_log_printf( const char *format, ... )
                 _init_stg_log_printf(err);
         }
 
+	
         fp = fopen( err, "a" );
+		if(fp==NULL) {
+			fp = stderr;
+		}	
 
         va_start(ap,format);
         vfprintf( fp, format, ap );
         va_end(ap);
 
-        fclose(fp);
+		if(fp != stderr)
+        	fclose(fp);
 
         beenhere = 1;
 #endif
@@ -97,12 +111,15 @@ FILE* _get_file_pointer_log_printf( void )
 	
 	_stg_log_write_filename( err );
 	fp = fopen( err, "a" );
-
+	if(fp==NULL) {
+		fp = stderr;
+	}
+	
 	return fp;
 }
 
 void stg_profile_EntryPoint( char ep_name[], char hk_name[], double time )
-{
+{ 
 #ifdef ENABLE_STGERMAIN_LOG
 	int len_ep, len_hk;
 	int max_length = 50;
@@ -164,7 +181,8 @@ void stg_profile_Func( char func_name[], double time )
                 }
 
                 fprintf( fp, " time= %6.6e (sec)\n", time );
-                fclose( fp );
+				if(fp != stderr)
+                	fclose( fp );
         }
         else {
                 /* do an unformatted */
