4923350e54885fc83f9614844f458b06a7ee6c50
LukeHodkinson <devnull@localhost>
2008-03-03 06:34:30 +0000

    Enabling use of 64 bit library directories.

--- config/packages/StgDomain.py
+++ config/packages/StgDomain.py
@@ -48,4 +48,8 @@ class StgDomain(SConfig.Package):
         if self.env['debug']:
             self.env.MergeFlags('-g')
 
+        # Setup 64 bit builds.
+        if platform.architecture()[0].find('64') != -1:
+            self.env.MergeFlags('-m64')
+
         return [1, '', '']
