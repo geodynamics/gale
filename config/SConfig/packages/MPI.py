fa6b127875ec026cad13ad58835da6bae61ae099
LukeHodkinson <devnull@localhost>
2008-03-06 01:27:08 +0000

    Adding the shared directory to library paths.

--- config/SConfig/packages/MPI.py
+++ config/SConfig/packages/MPI.py
@@ -4,10 +4,8 @@ import SConfig
 class MPI(SConfig.Package):
     def __init__(self, env, options):
         SConfig.Package.__init__(self, env, options)
-        self.header_sub_dir = 'mpi'
-        self.setup_search_defaults()
-        self.setup_options()
         self.base_patterns = ['mpich*', 'MPICH*']
+        self.header_sub_dir = 'mpi'
         self.headers = ['mpi.h']
         self.libraries = [['mpich'],
                           ['mpich', 'pmpich'],
@@ -16,3 +14,11 @@ class MPI(SConfig.Package):
                           ['mpi']]
         self.require_shared = True
         self.use_rpath = True
+
+    def validate_location(self, location):
+        for lib_dir in location[2]:
+            shared_dir = os.path.join(lib_dir, 'shared')
+            path = os.path.join(location[0], shared_dir)
+            if os.path.exists(path):
+                location[2] += [shared_dir]
+        return [1, '', '']
