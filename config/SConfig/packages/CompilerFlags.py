976854dba4242325ebd24d0206ca44dc27c20dfa
LukeHodkinson <devnull@localhost>
2008-03-08 10:28:19 +0000

    Automatically detecting which flags we can use with the current compiler. This really shouldn't be necessary; SCons is supposed to handle this kind of thing, but it turns out SCons isn't totally foolproof.

--- config/SConfig/packages/CompilerFlags.py
+++ config/SConfig/packages/CompilerFlags.py
@@ -5,7 +5,8 @@ import SConfig
 class CompilerFlags(SConfig.Package):
     def __init__(self, env, options):
         SConfig.Package.__init__(self, env, options)
-        self.checks = [self.check_architecture]
+        self.checks = [self.check_bit_flags,
+                       self.check_architecture]
 
     def setup_options(self):
         SConfig.Package.setup_options(self)
@@ -13,20 +14,42 @@ class CompilerFlags(SConfig.Package):
             return
         self.command_options += ['with_32bit']
         self.opts.AddOptions(
-            SCons.Script.BoolOption('with_32bit', 'Generate 32bit code', 0))
+            SCons.Script.BoolOption('with_32bit', 'Generate 32bit code', 0),
+            SCons.Script.BoolOption('with_64bit', 'Generate 64bit code', 0))
 
     def check_architecture(self):
         if (platform.platform().find('x86_64') != -1 or \
-                platform.platform().find('ppc64') != -1 or \
-                platform.architecture()[0].find('64') != -1) and \
-                not self.env['with_32bit']:
+            platform.platform().find('ppc64') != -1 or \
+            platform.architecture()[0].find('64') != -1 or \
+            self.env['with_64bit']) and \
+            not self.env['with_32bit']:
             self.bits = 64
-            self.env.MergeFlags('-m64')
-            if self.env.subst('$CC') == self.env.subst('$LINK'):
-                self.env.AppendUnique(LINKFLAGS='-m64')
+            if self.flag_64bit:
+                self.env.MergeFlags(self.flag_64bit)
+                if self.env.subst('$CC') == self.env.subst('$LINK'):
+                    self.env.AppendUnique(LINKFLAGS=[self.flag_64bit])
         else:
             self.bits = 32
-            self.env.MergeFlags('-m32')
-            if self.env.subst('$CC') == self.env.subst('$LINK'):
-                self.env.AppendUnique(LINKFLAGS='-m32')
+            if self.flag_32bit:
+                self.env.MergeFlags(self.flag_32bit)
+                if self.env.subst('$CC') == self.env.subst('$LINK'):
+                    self.env.AppendUnique(LINKFLAGS=[self.flag_32bit])
         return [1, '', '']
+
+    def check_bit_flags(self):
+        if self.try_flag('-m32')[0]:
+            self.flag_32bit = '-m32'
+        else:
+            self.flag_32bit = ''
+        if self.try_flag('-m64')[0]:
+            self.flag_64bit = '-m64'
+        else:
+            self.flag_64bit = ''
+        return [1, '', '']
+
+    def try_flag(self, flag):
+        state = self.env.ParseFlags(flag)
+        old = self.push_state(state)
+        result = self.run_scons_cmd(self.ctx.TryCompile, '', '.c')
+        self.pop_state(old)
+        return result
