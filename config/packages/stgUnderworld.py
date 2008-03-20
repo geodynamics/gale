5a0fe97560b596d34156bca8880050dfd098f3ab
LukeHodkinson <devnull@localhost>
2008-03-20 06:36:42 +0000

    Updating the stgUnderworld package.

--- config/packages/stgUnderworld.py
+++ config/packages/stgUnderworld.py
@@ -2,10 +2,9 @@ import os, platform
 import SConfig
 import SCons.Script
 
-class stgUnderworld(SConfig.Package):
+class stgUnderworld(SConfig.Node):
     def __init__(self, scons_env, scons_opts, required=False):
-        SConfig.Package.__init__(self, scons_env, scons_opts, required)
-        self.checks = [self.setup_environment]
+        SConfig.Node.__init__(self, scons_env, scons_opts, required)
         self.dependency(SConfig.packages.cmath)
         self.dependency(SConfig.packages.libXML2)
         self.dependency(SConfig.packages.MPI)
@@ -25,48 +24,51 @@ class stgUnderworld(SConfig.Package):
             self.dependency(SConfig.packages.libTIFF, False)
 
     def setup_options(self):
-        SConfig.Package.setup_options(self)
-        if self.opts:
-            self.opts.AddOptions(
-                SCons.Script.BoolOption('debug',
-                                        'Enable debugging', 1),
-                SCons.Script.BoolOption('static_libraries',
-                                        'Build static libraries', 0),
-                SCons.Script.BoolOption('shared_libraries',
-                                        'Build shared libraries', 1),
-                SCons.Script.BoolOption('with_glucifer', 'Enable the gLucifer module', 1),
-                ('buildPath', 'Temporary build path', 'build')
-                )
+        SConfig.Node.setup_options(self)
+        self.opts.AddOptions(
+            SCons.Script.BoolOption('debug',
+                                    'Enable debugging', 1),
+            SCons.Script.BoolOption('static_libraries',
+                                    'Build static libraries', 0),
+            SCons.Script.BoolOption('shared_libraries',
+                                    'Build shared libraries', 1),
+            SCons.Script.BoolOption('with_glucifer', 'Enable the gLucifer module', 1),
+            ('buildPath', 'Temporary build path', 'build')
+            )
+        self.option_map = {'debug': None,
+                           'static_libraries': None,
+                           'shared_libraries': None,
+                           'with_glucifer': None}
 
-    def setup_environment(self):
+    def enable(self, scons_env):
+        SConfig.Node.enable(self, scons_env)
+
         # Setup the build path.
-        if not os.path.isabs(self.env['buildPath']):
-            self.env['buildPath'] = '#' + self.env['buildPath']
+        if not os.path.isabs(scons_env['buildPath']):
+            scons_env['buildPath'] = '#' + scons_env['buildPath']
 
         # Setup LIB_DIR.
-        lib_dir = os.path.join(self.env['buildPath'], 'lib')
-        abs_lib_dir = self.env.Dir(lib_dir).abspath
-        self.env.AppendUnique(CPPDEFINES=[('LIB_DIR',
-                                           self.env['ESCAPE']('"' + abs_lib_dir + '"'))])
-        self.env.PrependUnique(LIBPATH=[lib_dir])
+        lib_dir = os.path.join(scons_env['buildPath'], 'lib')
+        abs_lib_dir = scons_env.Dir(lib_dir).abspath
+        scons_env.AppendUnique(CPPDEFINES=[('LIB_DIR',
+                                           scons_env['ESCAPE']('"' + abs_lib_dir + '"'))])
+        scons_env.PrependUnique(LIBPATH=[lib_dir])
 
         # Setup the RPATH.
-        self.env.PrependUnique(RPATH=[self.env.Dir(abs_lib_dir).abspath])
+        scons_env.PrependUnique(RPATH=[scons_env.Dir(abs_lib_dir).abspath])
 
         # Setup the module extension.
-        ext = self.env['ESCAPE']('"' + self.env['SHLIBSUFFIX'][1:] + '"')
-        self.env.Append(CPPDEFINES=[('MODULE_EXT', ext)])
+        ext = scons_env['ESCAPE']('"' + scons_env['SHLIBSUFFIX'][1:] + '"')
+        scons_env.Append(CPPDEFINES=[('MODULE_EXT', ext)])
 
         # Setup the include paths.
-        inc_path = os.path.join(self.env['buildPath'], 'include')
-        self.env.AppendUnique(CPPPATH=[inc_path])
+        inc_path = os.path.join(scons_env['buildPath'], 'include')
+        scons_env.AppendUnique(CPPPATH=[inc_path])
 
         # Setup debugging.
-        if self.env['debug']:
-            self.env.MergeFlags('-g')
+        if scons_env['debug']:
+            scons_env.MergeFlags('-g')
 
         # Setup the revision number.
-        ver = self.env['ESCAPE']('"' + str(self.svnrevision.revision) + '"')
-        self.env.Append(CPPDEFINES=[('VERSION', ver)])
-
-        return True
+        ver = scons_env['ESCAPE']('"' + str(self.svnrevision.revision) + '"')
+        scons_env.Append(CPPDEFINES=[('VERSION', ver)])
