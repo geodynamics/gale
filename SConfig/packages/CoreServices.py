779f6d66af23977fd8a922a45c93434c20da658a
JulianGiordani <devnull@localhost>
2008-08-29 01:40:28 +0000

    This ought to get Carbon compiled and in the mode for all you mac users out there.

--- SConfig/packages/CoreServices.py
+++ SConfig/packages/CoreServices.py
@@ -5,3 +5,4 @@ class CoreServices(SConfig.Package):
     def __init__(self, scons_env, scons_opts, required=False, **kw):
         SConfig.Package.__init__(self, scons_env, scons_opts, required, **kw)
         self.frameworks = ['CoreServices']
+        self.have_define = 'HAVE_CARBON'
