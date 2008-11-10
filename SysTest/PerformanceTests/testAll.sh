06f7809596b9abe2a0b0226557cc84ba86371248
Julian Giordani <julian.giordani@sci.monash.edu.au>
2008-11-10 11:20:45 +1100

    adding a script to test all analytics sols in one hit

---
+++ SysTest/PerformanceTests/testAll.sh
@@ -0,0 +1,5 @@
+./runAndTestConvergence.pl testVelicSolA.xml -optionsFile OFile.dat
+./runAndTestConvergence.pl testVelicSolB.xml -optionsFile OFile.dat
+./runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFile.dat
+./runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFile.dat
+./runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFile.dat 
