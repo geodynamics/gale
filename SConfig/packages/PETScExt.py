99f6947a00bdc81fbe49347859635ae3b096f24f
Julian Giordani <julian.giordani@sci.monash.edu.au>
2009-01-20 12:17:12 +1100

    Adding HAVE_PETScEXT - if PETScExt is found.

--- SConfig/packages/PETScExt.py
+++ SConfig/packages/PETScExt.py
@@ -15,6 +15,7 @@ class PETScExt(SConfig.Package):
         self.libraries = [['petscext_utils',
                            'petscext_snes', 'petscext_ksp', 'petscext_pc',
                            'petscext_mat', 'petscext_vec', 'petscext_helpers']]
+        self.have_define = 'HAVE_PETScEXT'
 
     def process_installation(self, inst):
         # Have to get the architecture using this garbage...
