76d027816eb2e23f16c4c0dfd83f31f6194b2244
Patrick Sunter <patdevelop@gmail.com>
2009-06-04 11:42:15 +0930

    Re-enabled MPIStreamSuite test - after fixing a couple of errors, the test now runs.
    Problem was that opening test files in parallel must be done carefully, and also had
    to put MPI_Barrier calls before test files are deleted, so each processor can assess
    the test file properly.

--- Base/IO/tests/MPIStreamSuite.c
+++ Base/IO/tests/MPIStreamSuite.c
@@ -31,24 +31,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #include "pcu/pcu.h"
 #include "StGermain/Base/Foundation/Foundation.h"
 #include "StGermain/Base/IO/IO.h"
 #include "MPIStreamSuite.h"
 
+#define MAXLINE 1000
+
 typedef struct {
-   MPI_Comm       CommWorld;
+   MPI_Comm       comm;
    int            rank;
-   int            numProcessors;
+   int            numProcs;
    Dictionary*    dict;
 } MPIStreamSuiteData;
 
 
 void MPIStreamSuite_Setup( MPIStreamSuiteData* data ) {
-   MPI_Comm_dup( MPI_COMM_WORLD, &data->CommWorld );
-   MPI_Comm_size( data->CommWorld, &data->numProcessors );
-   MPI_Comm_rank( data->CommWorld, &data->rank );
+   MPI_Comm_dup( MPI_COMM_WORLD, &data->comm );
+   MPI_Comm_size( data->comm, &data->numProcs );
+   MPI_Comm_rank( data->comm, &data->rank );
    data->dict = Dictionary_New();
 }
 
@@ -58,20 +61,15 @@ void MPIStreamSuite_Teardown( MPIStreamSuiteData* data ) {
    
 
 void MPIStreamSuite_TestWriteAllProcessors( MPIStreamSuiteData* data ) {
-   char        dataArray[13];
-   Index       ii;
-   Stream*     stream1;
-   const char* testMPIFilename = "./test-mpi1.txt";
-   FILE*       testMPIFile;
-   #define     MAXLINE 1000
-   char        outLine[MAXLINE];
-
-   Journal_Printf( Journal_Register( Error_Type, "MPIStreamSuite" ),
-      "Warning-MPIStreamSuite tests disabled since they currently produce segfaults,\n"
-      "seems to be a problem in the MPIStream or MPIFile code\n" );
-   pcu_check_true( 0 );
-   return;
-   /* Will not run below......................................................*/
+   Index          ii;
+   Stream*        stream1;
+   const char*    testMPIFilename = "./test-mpi1.txt";
+   FILE*          testMPIFile;
+   char           outLine[MAXLINE];
+   char           compString[MAXLINE];
+   #define        PER_RANK_COUNT 2
+   char           dataArray[PER_RANK_COUNT];
+   Index          rank_I;
 
    Dictionary_AddFromString( data->dict, "journal-file.MPIStream.one", testMPIFilename );
    Dictionary_AddFromUnsignedInt( data->dict, "journal-mpi-offset.MPIStream.one", 100 );
@@ -81,35 +79,54 @@ void MPIStreamSuite_TestWriteAllProcessors( MPIStreamSuiteData* data ) {
    pcu_check_true( STREAM_ALL_RANKS == Stream_GetPrintingRank( stream1 ) );
 
    /* Write half the alphabet to each process */
-   for ( ii = 0; ii < 13; ++ii ) {
-      dataArray[ii] = 'a' + 13 * data->rank + ii;
+   for ( ii = 0; ii < PER_RANK_COUNT; ++ii ) {
+      dataArray[ii] = 'a' + PER_RANK_COUNT * data->rank + ii;
    }
    
-   /* Print the alphabet */
-   MPIStream_WriteAllProcessors( stream1, dataArray, sizeof(char), 13, data->CommWorld );
+   /* Print the data */
+   MPIStream_WriteAllProcessors( stream1, dataArray, sizeof(char), PER_RANK_COUNT, data->comm );
    Stream_Flush( stream1 );
 
-   testMPIFile = fopen( testMPIFilename, "r" );
+   /* Now build up the comparison string. Depends on how many processes are running. */
+   for (rank_I=0; rank_I < data->numProcs; rank_I++ ) {
+      for ( ii = 0; ii < PER_RANK_COUNT; ++ii ) {
+         compString[PER_RANK_COUNT*rank_I+ii] = 'a' + PER_RANK_COUNT*rank_I + ii;
+      }
+   }
+   compString[data->numProcs*PER_RANK_COUNT] = '\0';
+
+   /* Do the following since in parallel on some systems, the file
+    * doesn't get re-opened at the start automatically. */
+   for ( rank_I = 0; rank_I < data->numProcs; rank_I++ ) {
+      MPI_Barrier( data->comm );
+      if ( rank_I == data->rank ) {
+         testMPIFile = fopen( testMPIFilename, "r" );
+         rewind( testMPIFile );
+      }
+   }
+
    pcu_check_true( fgets( outLine, MAXLINE, testMPIFile ));
-   pcu_check_true( 0 == strcmp( outLine, "abcdefghijklmnopqrstuvwxyz" ));
+   pcu_check_true( 0 == strcmp( outLine, compString ));
    fclose( testMPIFile );
-   remove( testMPIFilename );
+   /* Make sure not to delete the file until all processors have read from it */
+   MPI_Barrier( data->comm );
+   if ( data->rank == 0 ) {
+      remove( testMPIFilename );
+   }
 }
 
-   
+
 void MPIStreamSuite_TestPrintWithOffset( MPIStreamSuiteData* data ) {
-   Stream* stream2;
+   Stream*     stream2;
    const char* testMPIFilename = "./test-mpi2.txt";
    FILE*       testMPIFile;
-   #define     MAXLINE 1000
    char        outLine[MAXLINE];
-
-   Journal_Printf( Journal_Register( Error_Type, "MPIStreamSuite" ),
-      "Warning-MPIStreamSuite tests disabled since they currently produce segfaults,\n"
-      "seems to be a problem in the MPIStream or MPIFile code\n" );
-   pcu_check_true( 0 );
-   return;
-   /* Will not run below......................................................*/
+   char        rankPrintString[MAXLINE];
+   char        compString[MAXLINE];
+   Index       rank_I;
+   Index       ii;
+   Index       startPoint = 0;
+   unsigned    stringLength=0;
 
    Dictionary_AddFromString( data->dict, "journal-file.MPIStream.two", testMPIFilename );
    Journal_ReadFromDictionary( data->dict );
@@ -117,23 +134,47 @@ void MPIStreamSuite_TestPrintWithOffset( MPIStreamSuiteData* data ) {
    stream2 = Journal_Register( MPIStream_Type, "two" );
    pcu_check_true( STREAM_ALL_RANKS == Stream_GetPrintingRank( stream2 ) );
 
-   switch ( data->rank ) {
-      case 0:
-         MPIStream_SetOffset( stream2, 8, data->CommWorld );
-         Journal_Printf( stream2, "Hello %d", 10 );
-         break;
-      case 1:
-         MPIStream_SetOffset( stream2, 12, data->CommWorld );
-         Journal_Printf( stream2, "world %.3f\n", 0.123f );
-         break;
+   for( ii=0; ii <10*(data->rank+1); ii++ ) {
+      rankPrintString[ii] = 'a' + data->rank;
    }
+   rankPrintString[10*(data->rank+1)] = '\0';
+
+   stringLength = 10*(data->rank+1);
+
+   MPIStream_SetOffset( stream2, stringLength*sizeof(char), data->comm );
+
+   /* now print the data */
+   Journal_Printf( stream2, "%s", rankPrintString );
    Stream_Flush( stream2 );
 
-   testMPIFile = fopen( testMPIFilename, "r" );
+   /* Now build up the comparison string. Depends on how many processes are running. */
+   startPoint = 0;
+   for (rank_I=0; rank_I < data->numProcs; rank_I++ ) {
+      for ( ii = 0; ii < 10*(rank_I+1); ii++ ) {
+         compString[startPoint+ii] = 'a' + rank_I;
+      }
+      startPoint += 10*(rank_I+1);
+   }
+   compString[startPoint] = '\0';
+
+   /* Do the following since in parallel on some systems, the file
+    * doesn't get re-opened at the start automatically. */
+   for ( rank_I = 0; rank_I < data->numProcs; rank_I++ ) {
+      MPI_Barrier( data->comm );
+      if ( rank_I == data->rank ) {
+         testMPIFile = fopen( testMPIFilename, "r" );
+         rewind( testMPIFile );
+      }
+   }
+
    pcu_check_true( fgets( outLine, MAXLINE, testMPIFile ));
-   pcu_check_true( 0 == strcmp( outLine, "Hello 10world 0.123" ));
+   pcu_check_true( 0 == strcmp( outLine, compString ) );
    fclose( testMPIFile );
-   remove( testMPIFilename );
+   /* Make sure not to delete the file until all processors have read from it */
+   MPI_Barrier( data->comm );
+   if ( data->rank == 0 ) {
+      remove( testMPIFilename );
+   }
 }
 
 
