b90468af88eebe035dab3c5228d1f1d766c661ca
John Mansour <john.mansour@maths.monash.edu.au>
2009-06-26 14:28:26 +1000

    fully function XDML... not fully tested though

--- plugins/Output/XDMFGenerator/XDMFGenerator.c
+++ plugins/Output/XDMFGenerator/XDMFGenerator.c
@@ -142,7 +142,158 @@ void _Underworld_XDMFGenerator_GenerateAll( void* _context ) {
 }
 
 void _Underworld_XDMFGenerator_WriteFieldSchema( UnderworldContext* context, Stream* stream ) {
+   FieldVariable*       fieldVar = NULL;
+   FeVariable*          feVar    = NULL;
+   Mesh*                mesh;
+   unsigned             nDims;
+   unsigned             totalVerts;
+   Index                maxNodes;
+   Index                elementGlobalSize;
+   Element_GlobalIndex  gElement_I;
+	Index                var_I = 0;
+   Bool                 saveCoords  = Dictionary_GetBool_WithDefault( context->dictionary, "saveCoordsWithFields", False );
+   Stream*              errorStream = Journal_Register( Error_Type, CURR_MODULE_NAME );
+   Name                 variableType;
+   /** we follow the method in SystemSetup/Context.c to get the mesh info. 
+       this method seems unsatisfactory, and may be updated soon JohnMansour 20090626  **/ 
+   fieldVar = FieldVariable_Register_GetByIndex( context->fieldVariable_Register, 0 );
+   if ( Stg_Class_IsInstance( fieldVar, FeVariable_Type ) ) {
+      feVar  = (FeVariable*)fieldVar;
+      mesh   = (Mesh*)feVar->feMesh;
+      
+      nDims             = Mesh_GetDimSize( mesh );
+      totalVerts        = Mesh_GetGlobalSize( mesh, 0 );
+      elementGlobalSize = FeMesh_GetElementGlobalSize(mesh);
 
+      /* get connectivity array size */
+      if (mesh->nElTypes == 1)
+         maxNodes = FeMesh_GetElementNodeSize( mesh, 0);
+      else {
+         /* determine the maximum number of nodes each element has */
+         maxNodes = 0;
+         for ( gElement_I = 0 ; gElement_I < FeMesh_GetElementGlobalSize(mesh); gElement_I++ ) {
+            unsigned numNodes;
+            numNodes = FeMesh_GetElementNodeSize( mesh, gElement_I);
+            if( maxNodes < numNodes ) maxNodes = numNodes;
+         }
+      }
+      /** now write all the xdmf geometry info **/
+      /**----------------------- START GEOMETRY   ------------------------------------------------------------------------------------------------------------------- **/
+      /** first create the grid which is applicable to the checkpointed fevariables **/
+                              Journal_Printf( stream, "   <Grid Name=\"FeVariable_Grid\">\n\n" );
+                              Journal_Printf( stream, "      <Time Value=\"%f\" />\n\n", context->currentTime );
+      /** now print out topology info, only quadrilateral elements are supported at the moment **/
+                              Journal_Printf( stream, "         <Topology Type=\"Quadrilateral\" NumberOfElements=\"%u\"> \n", elementGlobalSize );
+                              Journal_Printf( stream, "            <DataItem Format=\"HDF\" DataType=\"Int\"  Dimensions=\"%u %u\">Mesh.%05d.h5:/connectivity</DataItem>\n", elementGlobalSize, maxNodes, context->timeStep );
+                              Journal_Printf( stream, "         </Topology>\n\n" );
+                              Journal_Printf( stream, "         <Geometry Type=\"XYZ\">\n" );
+
+      Stg_asprintf( &variableType, "NumberType=\"Float\" Precision=\"8\"" );                              
+      if( nDims == 2 ){
+         /** note that for 2d, we feed back a quasi 3d array, with the 3rd Dof zeroed.  so in effect we always work in 3d.
+             this is done because paraview in particular seems to do everything in 3d, and if you try and give it a 2d vector 
+             or array, it complains.... and hence the verbosity of the following 2d definitions**/
+                              Journal_Printf( stream, "            <DataItem ItemType=\"Function\"  Dimensions=\"%u 3\" Function=\"JOIN($0, $1, 0*$1)\">\n", totalVerts );
+                              Journal_Printf( stream, "               <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"XCoords\">\n", totalVerts );
+                              Journal_Printf( stream, "                  <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 0 1 1 %u 1 </DataItem>\n", totalVerts );
+                              Journal_Printf( stream, "                  <DataItem Format=\"HDF\" %s Dimensions=\"%u 2\">Mesh.%05d.h5:/data</DataItem>\n", variableType, totalVerts, context->timeStep );
+                              Journal_Printf( stream, "               </DataItem>\n" );
+                              Journal_Printf( stream, "               <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"YCoords\">\n", totalVerts );
+                              Journal_Printf( stream, "                  <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 1 1 1 %u 1 </DataItem>\n", totalVerts );
+                              Journal_Printf( stream, "                  <DataItem Format=\"HDF\" %s Dimensions=\"%u 2\">Mesh.%05d.h5:/data</DataItem>\n", variableType, totalVerts, context->timeStep );
+                              Journal_Printf( stream, "               </DataItem>\n" );
+                              Journal_Printf( stream, "            </DataItem>\n" );
+      } else if ( nDims == 3 ) {
+         /** in 3d we simply feed back the 3d hdf5 array, nice and easy **/
+                              Journal_Printf( stream, "            <DataItem Format=\"HDF\" %s Dimensions=\"%u 3\">Mesh.%05d.h5:/data</DataItem>\n", variableType, totalVerts, context->timeStep );
+      } else {
+         Journal_Firewall( NULL, errorStream, "\n\n Position SwarmVariable is not of dofCount 2 or 3.  Should this swarm be checkpointed?\n\n" );
+      }
+                              Journal_Printf( stream, "         </Geometry>\n\n" );
+      /**----------------------- FINISH GEOMETRY  ------------------------------------------------------------------------------------------------------------------- **/
+                
+                              
+   }
+
+   /** now write FeVariable data **/   
+	for ( var_I = 0; var_I < context->fieldVariable_Register->objects->count; var_I++ ) {
+		fieldVar = FieldVariable_Register_GetByIndex( context->fieldVariable_Register, var_I );
+
+		if ( Stg_Class_IsInstance( fieldVar, FeVariable_Type ) ) {
+			feVar = (FeVariable*)fieldVar;
+            if ( feVar->isCheckpointedAndReloaded ) {
+                  Name   variableType;
+                  Name   centering;
+                  Index  offset = 0;
+                  Index  meshSize = Mesh_GetGlobalSize( feVar->feMesh, 0 );
+                  Index  dofCountIndex;
+                  Index  dofAtEachNodeCount;
+
+            /**----------------------- START ATTRIBUTES ------------------------------------------------------------------------------------------------------------------- **/
+                  /** if coordinates are being stored with feVariable, account for this **/
+                  if( saveCoords) offset = nDims; 
+                  /** all feVariables are currently stored as doubles **/
+                  Stg_asprintf( &variableType, "NumberType=\"Float\" Precision=\"8\"" );
+                  /** determine whether feVariable data is cell centered (like Pressure), or on the nodes (like Velocity) **/
+                  if( meshSize == elementGlobalSize ){
+                     Stg_asprintf( &centering, "Cell" );
+                  } else if( meshSize == totalVerts ) {
+                     Stg_asprintf( &centering, "Node" );
+                  } else {
+                     Journal_Firewall( NULL, errorStream, "\n\n mesh global size must equal number of nodes, or number of elements... something is amiss! \n\n" );
+                  }
+                  /** how many degrees of freedom does the fevariable have? **/
+                  dofAtEachNodeCount = feVar->fieldComponentCount;
+                  if ( dofAtEachNodeCount == 1 ) {
+                              Journal_Printf( stream, "         <Attribute Type=\"Scalar\" Center=\"%s\" Name=\"%s\">\n", centering,  feVar->name);
+                              Journal_Printf( stream, "            <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" >\n", meshSize );
+                              Journal_Printf( stream, "               <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 1 </DataItem>\n", offset, meshSize );
+                              Journal_Printf( stream, "               <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d.h5:/data</DataItem>\n", variableType, meshSize, (offset + dofAtEachNodeCount), feVar->name, context->timeStep);
+                              Journal_Printf( stream, "            </DataItem>\n" );
+                              Journal_Printf( stream, "         </Attribute>\n\n" );
+                  } else if( dofAtEachNodeCount == 2 ){
+                     /** note that for 2d, we feed back a quasi 3d array, with the 3rd Dof zeroed.  so in effect we always work in 3d.
+                         this is done because paraview in particular seems to do everything in 3d, and if you try and give it a 2d vector 
+                         or array, it complains.... and hence the verbosity of the following 2d definitions **/
+                              Journal_Printf( stream, "         <Attribute Type=\"Vector\" Center=\"%s\" Name=\"%s\">\n", centering,  feVar->name);
+                              Journal_Printf( stream, "            <DataItem ItemType=\"Function\"  Dimensions=\"%u 3\" Function=\"JOIN($0, $1, 0*$1)\">\n", meshSize );
+                              Journal_Printf( stream, "               <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"XValue\">\n", meshSize );
+                              Journal_Printf( stream, "                  <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 1 </DataItem>\n", offset, meshSize );
+                              Journal_Printf( stream, "                  <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d.h5:/data</DataItem>\n", variableType, meshSize, (offset + dofAtEachNodeCount), feVar->name, context->timeStep);
+                              Journal_Printf( stream, "               </DataItem>\n" );
+                              Journal_Printf( stream, "               <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"YValue\">\n", meshSize );
+                              Journal_Printf( stream, "                  <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 1 </DataItem>\n", (offset+1), meshSize );
+                              Journal_Printf( stream, "                  <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d.h5:/data</DataItem>\n", variableType, meshSize, (offset + dofAtEachNodeCount), feVar->name, context->timeStep);
+                              Journal_Printf( stream, "               </DataItem>\n" );
+                              Journal_Printf( stream, "            </DataItem>\n" );
+                              Journal_Printf( stream, "         </Attribute>\n\n" );
+                  } else if ( dofAtEachNodeCount == 3 ) {
+                     /** in 3d we simply feed back the 3d hdf5 array, nice and easy **/
+                              Journal_Printf( stream, "         <Attribute Type=\"Vector\" Center=\"%s\" Name=\"%s\">\n", centering,  feVar->name);
+                              Journal_Printf( stream, "            <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 3\" >\n", meshSize );
+                              Journal_Printf( stream, "               <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 3 </DataItem>\n", offset, meshSize );
+                              Journal_Printf( stream, "               <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d.h5:/data</DataItem>\n", variableType, meshSize, (offset + dofAtEachNodeCount), feVar->name, context->timeStep);
+                              Journal_Printf( stream, "            </DataItem>\n" );
+                              Journal_Printf( stream, "         </Attribute>\n\n" );
+                  } else {
+                     /** where there are more than 3 components, we write each one out as a scalar **/
+                     for(dofCountIndex = 0 ; dofCountIndex < dofAtEachNodeCount ; ++dofCountIndex){
+                              Journal_Printf( stream, "         <Attribute Type=\"Scalar\" Center=\"%s\" Name=\"%s-Component-%u\">\n", centering,  feVar->name, dofCountIndex);
+                              Journal_Printf( stream, "            <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" >\n", meshSize );
+                              Journal_Printf( stream, "               <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 1 </DataItem>\n", (offset+dofCountIndex), meshSize );
+                              Journal_Printf( stream, "               <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d.h5:/data</DataItem>\n", variableType, meshSize, (offset + dofAtEachNodeCount), feVar->name, context->timeStep);
+                              Journal_Printf( stream, "            </DataItem>\n" );
+                              Journal_Printf( stream, "         </Attribute>\n" );
+                     }
+                              Journal_Printf( stream, "\n" );
+                  }
+            /**----------------------- END ATTRIBUTES   ------------------------------------------------------------------------------------------------------------------- **/
+            Memory_Free( centering );
+            }
+      }
+   }
+                              Journal_Printf( stream, "   </Grid>\n\n" );
+            Memory_Free( variableType );         
 }
 
 void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Stream* stream ) {
@@ -161,7 +312,7 @@ void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Str
    Name            filename_part;
    Stream*         errorStream  = Journal_Register( Error_Type, CURR_MODULE_NAME );
 	const int       FINISHED_WRITING_TAG = 100;
-	MPI_Status status;
+	MPI_Status      status;
    
    /** get total number of different swarms **/
    swarmCount  = swarmRegister->swarmList->count;
@@ -184,7 +335,7 @@ void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Str
       for (ii = 0 ; ii < context->nproc ; ++ii) {
 
          /** get the number of particles in each swarm from each process **/
-         if (ii != 0             ) MPI_Recv( &swarmParticleLocalCount, 1, MPI_INT, ii, FINISHED_WRITING_TAG, context->communicator, &status );
+         if (ii != MASTER        ) MPI_Recv( &swarmParticleLocalCount, 1, MPI_INT, ii, FINISHED_WRITING_TAG, context->communicator, &status );
          if (context->nproc != 1 ) Stg_asprintf( &filename_part, ".%uof%u", (ii + 1), context->nproc );
          
          /** first write all the MASTER procs swarm info **/
@@ -201,7 +352,7 @@ void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Str
             Memory_Free( swarmVarName );
             
             /** check what precision it Position variable is stored at **/
-             if( swarmVar->variable->dataTypes[0] == Variable_DataType_Int ){
+            if( swarmVar->variable->dataTypes[0] == Variable_DataType_Int ){
                Journal_Firewall( NULL, errorStream, "\n\n Position variable can not be of type Int... something is amiss! \n\n" );
             } else if( swarmVar->variable->dataTypes[0] == Variable_DataType_Char){
                Journal_Firewall( NULL, errorStream, "\n\n Position variable can not be of type Char... something is amiss! \n\n" );
@@ -258,8 +409,7 @@ void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Str
                   } else if( swarmVar->dofCount == 2 ){
                      /** note that for 2d, we feed back a quasi 3d array, with the 3rd Dof zeroed.  so in effect we always work in 3d.
                          this is done because paraview in particular seems to do everything in 3d, and if you try and give it a 2d vector 
-                         or array, it complains.... and hence the verbosity of the following 2d definitions
-                         we also assume that all swarmVariables of dof 2 or 3 are vectors, which may not always be appropriate **/
+                         or array, it complains.... and hence the verbosity of the following 2d definitions **/
                               Journal_Printf( stream, "         <Attribute Type=\"Vector\" Center=\"Node\" Name=\"%s\">\n", swarmVar->name);
                               Journal_Printf( stream, "            <DataItem ItemType=\"Function\"  Dimensions=\"%u 3\" Function=\"JOIN($0, $1, 0*$1)\">\n", swarmParticleLocalCount );
                               Journal_Printf( stream, "               <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"XValue\">\n", swarmParticleLocalCount );
@@ -281,7 +431,7 @@ void _Underworld_XDMFGenerator_WriteSwarmSchema( UnderworldContext* context, Str
                      /** where there are more than 3 components, we write each one out as a scalar **/
                      for(dofCountIndex = 0 ; dofCountIndex < swarmVar->dofCount ; ++dofCountIndex){
                               Journal_Printf( stream, "         <Attribute Type=\"Scalar\" Center=\"Node\" Name=\"%s-Component-%u\">\n", swarmVar->name, dofCountIndex);
-                              Journal_Printf( stream, "            <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" Name=\"XValue\">\n", swarmParticleLocalCount );
+                              Journal_Printf( stream, "            <DataItem ItemType=\"HyperSlab\" Dimensions=\"%u 1\" >\n", swarmParticleLocalCount );
                               Journal_Printf( stream, "               <DataItem Dimensions=\"3 2\" Format=\"XML\"> 0 %u 1 1 %u 1 </DataItem>\n", dofCountIndex, swarmParticleLocalCount );
                               Journal_Printf( stream, "               <DataItem Format=\"HDF\" %s Dimensions=\"%u %u\">%s.%05d%s.h5:/%s</DataItem>\n", variableType, swarmParticleLocalCount, swarmVar->dofCount, currentSwarm->name, context->timeStep, filename_part, swarmVar->name);
                               Journal_Printf( stream, "            </DataItem>\n" );
