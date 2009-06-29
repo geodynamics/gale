7f7cae93f3f631d3ea4593f0e5fc0231ca8451a0
owen <owen@dyn-130-194-220-234.its.monash.edu.au>
2009-06-29 16:21:00 +1000

    X11 config fix for mac

--- packages/X11.py
+++ packages/X11.py
@@ -5,19 +5,19 @@ class X11(Package):
     def gen_locations(self):
         yield ('/usr', ['/usr/include/X11'], ['/usr/lib'])
         yield ('/usr/X11R6', ['/usr/X11R6/include'], ['/usr/X11R6/lib'])
-        yield ('/usr/X11R6', ['/usr/X11R6/include/X11'], ['/usr/X11R6/lib'])
+        yield ('/usr/X11R6', ['/usr/X11R6/include/X11', '/usr/X11R6/include'], ['/usr/X11R6/lib'])
         yield ('/usr/local', ['/usr/local'], ['/usr/local'])
         yield ('/usr/local', ['/usr/local/X11'], ['/usr/local'])
 
     def gen_envs(self, loc):
         for env in Package.gen_envs(self, loc):
-            env['pkg_headers'] = ['Xlib.h']
+            env['pkg_headers'] = ['Xlib.h', 'GL/glx.h']
             lib_env = env.Clone()
-            lib_env.PrependUnique(LIBS=['X11', 'Xmu'])
+            lib_env.PrependUnique(LIBS=['X11', 'Xmu', 'GL'])
             yield lib_env
             lib_env = env.Clone()
-            lib_env.PrependUnique(LIBS=['X11', 'Xmu', 'Xau', 'Xdmcp'])
+            lib_env.PrependUnique(LIBS=['X11', 'Xmu', 'GL', 'Xau', 'Xdmcp'])
             yield lib_env
             lib_env = env.Clone()
-            lib_env.PrependUnique(LIBS=['X11', 'Xmu', 'xcb', 'xcb-xlib', 'Xau', 'Xdmcp'])
+            lib_env.PrependUnique(LIBS=['X11', 'Xmu', 'GL', 'xcb', 'xcb-xlib', 'Xau', 'Xdmcp'])
             yield lib_env
