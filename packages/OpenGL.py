e6b0df68c57168a87075a90647fc02c54590b0d6
JohnMansour <devnull@localhost>
2009-07-03 03:57:46 +0100

    added some new locations to check for headers/libs, and fixed others.

--- packages/OpenGL.py
+++ packages/OpenGL.py
@@ -5,10 +5,16 @@ class OpenGL(Package):
 
     def gen_locations(self):
         yield ('/usr', ['/usr/include/GL'], ['/usr/lib'])
+        yield ('/usr', ['/usr/include/GL'], ['/usr/X11R6/lib'])
+        yield ('/usr', ['/usr/include/GL'], ['/usr/X11R6/lib64'])
         yield ('/usr/X11R6', ['/usr/X11R6/include'], ['/usr/X11R6/lib'])
         yield ('/usr/X11R6', ['/usr/X11R6/include/GL'], ['/usr/X11R6/lib'])
-        yield ('/usr/local', ['/usr/local'], ['/usr/local'])
-        yield ('/usr/local', ['/usr/local/GL'], ['/usr/local'])
+        yield ('/usr/X11R6', ['/usr/X11R6/include'], ['/usr/X11R6/lib64'])
+        yield ('/usr/X11R6', ['/usr/X11R6/include/GL'], ['/usr/X11R6/lib64'])
+        yield ('/usr/local', ['/usr/local/include'], ['/usr/local/lib'])
+        yield ('/usr/local', ['/usr/local/include/GL'], ['/usr/local/lib'])
+        yield ('/usr/local', ['/usr/local/include'], ['/usr/local/lib64'])
+        yield ('/usr/local', ['/usr/local/include/GL'], ['/usr/local/lib64'])
 
     def gen_envs(self, loc):
         for env in Package.gen_envs(self, loc):
