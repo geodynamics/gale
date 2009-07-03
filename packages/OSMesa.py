12796446a201848b165b6c1df1e59ff3c6b8b1a9
John Mansour <john.mansour@maths.monash.edu.au>
2009-07-03 11:28:00 +1000

    further mods to OSMesa to include check for lib64 directories, also some more corrections (same for OpenGL).

--- packages/OSMesa.py
+++ packages/OSMesa.py
@@ -5,7 +5,9 @@ class OSMesa(Package):
     def gen_locations(self):
         yield ('/usr', ['/usr/include'], ['/usr/lib'])
         yield ('/usr/X11R6', ['/usr/X11R6/include'], ['/usr/X11R6/lib'])
-        yield ('/usr/local', ['/usr/local/include'], ['/usr/local'])
+        yield ('/usr/X11R6', ['/usr/X11R6/include'], ['/usr/X11R6/lib64'])
+        yield ('/usr/local', ['/usr/local/include'], ['/usr/local/lib'])
+        yield ('/usr/local', ['/usr/local/include'], ['/usr/local/lib64'])
 
     def gen_envs(self, loc):
         for env in Package.gen_envs(self, loc):
