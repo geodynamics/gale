b8670fa793721e9ddfa36a3c79b903b1f132d32b
LukeHodkinson <luke@vpac.org>
2009-07-15 10:09:11 +1000

    Fixed a problem with stgGlobus.

--- packages/stgGlobus.py
+++ packages/stgGlobus.py
@@ -4,15 +4,13 @@ from config import Package
 class stgGlobus(Package):
 
     def gen_locations(self):
-        yield ('/usr',
-               ['/usr/include/gcc32dbg', '/usr/include/gcc32dbgpthr'],
-               ['/usr/lib'])
-        yield ('/usr/local',
-               ['/usr/local/include/gcc32dbg', '/usr/local/include/gcc32dbgpthr'],
-               ['/usr/local/lib'])
+        yield ('/usr', [], [])
+        yield ('/usr/local', [], [])
 
-    def gen_base_extensions(self, base):
-        yield (['include/gcc32dbg', 'include/gcc32dbgpthr'], 'lib')
+    def gen_base_extensions(self):
+        for e in Package.gen_base_extensions(self):
+            yield e
+            yield (['include/gcc32dbg', 'include/gcc32dbgpthr'], 'lib')
 
     def gen_envs(self, loc):
         libs = "globus_io_gcc32dbg globus_ftp_control_gcc32dbg globus_ftp_client_gcc32dbg globus_gass_transfer_gcc32dbg globus_wsrf_core_tools_gcc32dbg globus_c_wsrf_resource_gcc32dbg globus_wsa_bindings_gcc32dbg globus_ds_bindings_gcc32dbg globus_wsu_bindings_gcc32dbg globus_wsp_bindings_gcc32dbg globus_wst_bindings_gcc32dbg globus_wsse_bindings_gcc32dbg globus_wsseu_bindings_gcc32dbg globus_wsc_bindings_gcc32dbg globus_wsbf_bindings_gcc32dbg globus_wsnt_bindings_gcc32dbg globus_wsrl_bindings_gcc32dbg globus_wsrp_bindings_gcc32dbg globus_wssg_bindings_gcc32dbg globus_notification_consumer_bindings_gcc32dbg globus_subscription_manager_bindings_gcc32dbg globus_ws_messaging_gcc32dbg globus_usage_gcc32dbg globus_xio_gcc32dbg gssapi_error_gcc32dbg globus_gss_assist_gcc32dbg globus_gssapi_gsi_gcc32dbg globus_gsi_proxy_core_gcc32dbg globus_gsi_credential_gcc32dbg globus_gsi_callback_gcc32dbg globus_oldgaa_gcc32dbg globus_gsi_sysconfig_gcc32dbg globus_gsi_cert_utils_gcc32dbg globus_openssl_gcc32dbg globus_openssl_error_gcc32dbg globus_callout_gcc32dbg globus_proxy_ssl_gcc32dbg globus_common_gcc32dbg ssl_gcc32dbg crypto_gcc32dbg xml2_gcc32dbg ltdl_gcc32dbg"
