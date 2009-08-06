df84ba2b902f80db3e02de0257e509fe43b63240
JulianGiordani <devnull@localhost>
2009-08-06 15:54:04 +1000

    finishing the testBox suite

--- Shape/tests/ShapeSuite.c
+++ Shape/tests/ShapeSuite.c
@@ -15,33 +15,34 @@ void ShapeSuite_Setup( ShapeSuiteData* data ) { }
 void ShapeSuite_Teardown( ShapeSuiteData* data ) {
 	//Stg_Component_Destroy( data->context, 0 /* dummy */, False );
 	//Stg_Class_Delete( data->context );
+	/* remove generated dat file */
+	//if( remove("output/test.dat") != 0 ) { pcu_assert(0); }
+	//if( remove("output/input.xml") != 0 ) { pcu_assert(0); }
+	//remove("output/input.xml");
 }
 
 
-void ShapeSuite_TestBox2D( ShapeSuiteData* data ) {
+void ShapeSuite_GenerateBox( ShapeSuiteData* data, Dimension_Index dim, char* inputFileName ) {
 /** Test Definition: */
 	DomainContext*   context = NULL;
 	Dictionary*      dictionary;
 	Stg_Shape*       shape;
 	unsigned         testCoordCount, index;
 	Name             outputPath;
-	Dimension_Index  dim;
 	Coord            coord;
-	Stream*          stream = Journal_Register( Info_Type, Stg_Shape_Type );
+	Stream*          stream = Journal_Register( Info_Type, inputFileName );
 	char xml_input[PCU_PATH_MAX];
-	char expected_file[PCU_PATH_MAX];
 
 	/* read in the xml input file */
-	pcu_filename_input( "testBox2D.xml", xml_input );
+	pcu_filename_input( inputFileName, xml_input );
 	context = (DomainContext*)stgMainInitFromXML( xml_input, MPI_COMM_WORLD );
 	dictionary = context->dictionary;
 	outputPath = Dictionary_GetString( dictionary, "outputPath" );
 	Stream_RedirectFile_WithPrependedPath( stream, outputPath, "test.dat" );
-	shape   = (Stg_Shape*) LiveComponentRegister_Get( context->CF->LCRegister, "shape" );
+	shape = (Stg_Shape*) LiveComponentRegister_Get( context->CF->LCRegister, "shape" );
 	assert( shape );
 
 	testCoordCount = Dictionary_GetUnsignedInt_WithDefault( dictionary, "testCoordCount", 10000 );
-	dim = Dictionary_GetUnsignedInt( dictionary, "dim" );
 
 	/* Test to see if random points are in shape */
 	srand48(0);
@@ -55,16 +56,31 @@ void ShapeSuite_TestBox2D( ShapeSuiteData* data ) {
 			Journal_Printf( stream, "%u\n", index );
 	}
 
+	Stream_CloseAndFreeFile( stream );
+}
+
+void ShapeSuite_TestBox2D( ShapeSuiteData* data ) {
+	Dimension_Index  dim = 2;
+	char expected_file[PCU_PATH_MAX];
+	ShapeSuite_GenerateBox( data, dim, "testBox2D.xml" );
+	
 	pcu_filename_expected( "testBox2D.expected", expected_file );
 	pcu_check_fileEq( "output/test.dat", expected_file );
+	remove("output/test.dat");
+}
+void ShapeSuite_TestBox3D( ShapeSuiteData* data ) {
+	Dimension_Index  dim = 3;
+	char expected_file[PCU_PATH_MAX];
+	ShapeSuite_GenerateBox( data, dim, "testBox3D.xml" );
 
-	/* remove generated dat file */
-	if( remove("output/test.dat") != 0 ) { pcu_assert(0); }
-	if( remove("output/input.xml") != 0 ) { pcu_assert(0); }
+	pcu_filename_expected( "testBox3D.expected", expected_file );
+	pcu_check_fileEq( "output/test.dat", expected_file );
+	remove("output/test.dat");
 }
 
 void ShapeSuite( pcu_suite_t* suite ) {
    pcu_suite_setData( suite, ShapeSuiteData );
    pcu_suite_setFixtures( suite, ShapeSuite_Setup, ShapeSuite_Teardown );
    pcu_suite_addTest( suite, ShapeSuite_TestBox2D );
+   pcu_suite_addTest( suite, ShapeSuite_TestBox3D );
 }
