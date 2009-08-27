e9774c00b9a521a0d1df209335d379a08ef09e9e
JericoRevote <devnull@localhost>
2009-08-27 13:54:20 +1000

    Added PCU test name space to the quadratic, exponential functions. Added CompositeVCSuite PCU test suite. Added CompositeVCSuite input and expected files. Purged old testCompositeVC* expected files and shell scripts.

--- Utils/tests/CompositeVCSuite.c
+++ Utils/tests/CompositeVCSuite.c
@@ -25,7 +25,7 @@
 **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 **
 ** Role:
-**   Tests the AllNodesVCSuite
+**   Tests the CompositeVCSuite
 **
 ** $Id: testTemplate.c 3462 2006-02-19 06:53:24Z WalterLandry $
 **
@@ -42,26 +42,32 @@
 #include "StgDomain/Utils/Utils.h"
 #include "StgDomain/Swarm/Swarm.h"
 
-#include "AllNodesVCSuite.h"
+#include "CompositeVCSuite.h"
 
 typedef struct {
-	MPI_Comm	comm;
+	MPI_Comm comm;
 	unsigned rank;
 	unsigned nProcs;
-} AllNodesVCSuiteData;
+} CompositeVCSuiteData;
 
-void AllNodesVCSuite_quadratic(Index index, Variable_Index var_I, void* context, void* result) {
-	*(double *)result = 20.0;
+void CompositeVCSuite_quadratic(Index index, Variable_Index var_I, void* context, void* result) {
+   *(double *)result = 20.0;
 }
 
-Mesh* AllNodesVCSuite_buildMesh( unsigned nDims, unsigned* size, double* minCrds, double* maxCrds, ExtensionManager_Register* emReg ) {
+void CompositeVCSuite_exponential(Index index, Variable_Index var_I, void* context, void* result) {
+   *(double *)result = 30.0;
+}
+
+
+Mesh* CompositeVCSuite_buildMesh( unsigned nDims, unsigned* size, double* minCrds, double* maxCrds, ExtensionManager_Register* emReg ) {
 	CartesianGenerator*	gen;
 	Mesh*						mesh;
+	unsigned					maxDecomp[3] = {0, 1, 1};
 
 	gen = CartesianGenerator_New( "" );
 	gen->shadowDepth = 0;
-	CartesianGenerator_SetDimSize( gen, nDims ); 
-	CartesianGenerator_SetTopologyParams( gen, size, 0, NULL, NULL );
+	CartesianGenerator_SetDimSize( gen, nDims );
+	CartesianGenerator_SetTopologyParams( gen, size, 0, NULL, maxDecomp );
 	CartesianGenerator_SetGeometryParams( gen, minCrds, maxCrds );
 
 	mesh = Mesh_New( "" );
@@ -76,53 +82,55 @@ Mesh* AllNodesVCSuite_buildMesh( unsigned nDims, unsigned* size, double* minCrds
 	return mesh;
 }
 
-void AllNodesVCSuite_Setup( AllNodesVCSuiteData* data ) {
+void CompositeVCSuite_Setup( CompositeVCSuiteData* data ) {
 	/* MPI Initializations */
 	data->comm = MPI_COMM_WORLD;
 	MPI_Comm_rank( data->comm, &data->rank );
 	MPI_Comm_size( data->comm, &data->nProcs );
 }
 
-void AllNodesVCSuite_Teardown( AllNodesVCSuiteData* data ) {
+void CompositeVCSuite_Teardown( CompositeVCSuiteData* data ) {
 }
 
-void AllNodesVCSuite_TestAllNodesVC( AllNodesVCSuiteData* data ) {
-	unsigned								nDomains;
+void CompositeVCSuite_TestCompositeVC( CompositeVCSuiteData* data ) {
+	Stream*								stream;
+	Dictionary*							dictionary;
+	XML_IO_Handler*					io_handler;
 	unsigned								nDims = 3;
 	unsigned								meshSize[3] = {3, 3, 3};
-	int									procToWatch;
+   unsigned								procToWatch;
 	double								minCrds[3] = {0.0, 0.0, 0.0};
 	double								maxCrds[3] = {1.0, 1.0, 1.0};
-	double*								array[7];
-	char*									vcKey = "AllNodesVC";
-	char*									varName[] = {"x", "y", "z", "vx", "vy", "vz", "temp"};
-	char									input_file[PCU_PATH_MAX];
-	char									expected_file[PCU_PATH_MAX];
 	Mesh*									mesh;
+	Variable*							var[7];
 	Variable_Register*				variable_Register;
+	WallVC*								vc;
+	CompositeVC*						cvc;
 	ConditionFunction*				quadCF;
+	ConditionFunction*				expCF;
 	ConditionFunction_Register*	conFunc_Register;
 	ExtensionManager_Register*		extensionMgr_Register;
-	DomainContext*						context;
-	Dictionary*							dictionary;
-	Stream*								stream;
-	XML_IO_Handler*					io_handler;
-	Variable*							var[7];
-	VariableCondition*				vc; 
-	Index									i, j, k;
+   double*								array[7];
+   char*									vcKey[] = {"WallVC_Front", "WallVC_Back", "WallVC_Left", "WallVC_Right", "WallVC_Top", "WallVC_Bottom"};
+   char*									vcKeyName[] = {"WallVC_FrontName", "WallVC_BackName", "WallVC_LeftName", "WallVC_RightName", "WallVC_TopName", "WallVC_BottomName"};
+   char*									varName[] = {"x", "y", "z", "vx", "vy", "vz", "temp"};
+	char									input_file[PCU_PATH_MAX];
+	char									expected_file[PCU_PATH_MAX];
+   unsigned								nDomains;
+   Index									i, j, k;
 
 	procToWatch = data->nProcs >=2 ? 1 : 0;
 
    io_handler = XML_IO_Handler_New();
 
- 	stream = Journal_Register( Info_Type, "AllNodesVCStream" );
-   Stream_RedirectFile( stream, "testAllNodesVC.dat" );
+ 	stream = Journal_Register( Info_Type, "CompositeVCStream" );
+   Stream_RedirectFile( stream, "testCompositeVC.dat" );
 
    dictionary = Dictionary_New();
    Dictionary_Add(dictionary, "outputPath", Dictionary_Entry_Value_FromString("./output"));
 
 	/* Input file */
-	pcu_filename_input( "allVC.xml", input_file );
+	pcu_filename_input( "wallVC.xml", input_file );
    IO_Handler_ReadAllFromFile(io_handler, input_file, dictionary);
    fflush(stdout);
 
@@ -133,16 +141,18 @@ void AllNodesVCSuite_TestAllNodesVC( AllNodesVCSuiteData* data ) {
    nDomains = Mesh_GetDomainSize( mesh, MT_VERTEX );
 
    /* Create CF stuff */
-   quadCF = ConditionFunction_New(AllNodesVCSuite_quadratic, "quadratic");
-   conFunc_Register = ConditionFunction_Register_New();
-   ConditionFunction_Register_Add(conFunc_Register, quadCF);
+	quadCF = ConditionFunction_New(CompositeVCSuite_quadratic, "quadratic");
+	expCF = ConditionFunction_New(CompositeVCSuite_exponential, "exponential");
+	conFunc_Register = ConditionFunction_Register_New();
+	ConditionFunction_Register_Add(conFunc_Register, quadCF);
+	ConditionFunction_Register_Add(conFunc_Register, expCF);
 
 	/* Create variable register */
    variable_Register = Variable_Register_New();
 
-   /* Create variables */
+	/* Create variables */
    for (i = 0; i < 6; i++) {
-		array[i] = Memory_Alloc_Array( double, nDomains, "array[i]" );
+      array[i] = Memory_Alloc_Array( double, nDomains, "array[i]" );
       var[i] = Variable_NewScalar( varName[i], Variable_DataType_Double, &nDomains, NULL, (void**)&array[i], 0 );
       Variable_Register_Add(variable_Register, var[i]);
    }
@@ -151,65 +161,71 @@ void AllNodesVCSuite_TestAllNodesVC( AllNodesVCSuiteData* data ) {
    Variable_Register_Add(variable_Register, var[6]);
    Variable_Register_BuildAll(variable_Register);
 
-   /* Create AllVC */
-   vc = (VariableCondition*)AllNodesVC_New( "AllNodesVC", vcKey, variable_Register, conFunc_Register, dictionary, mesh );
-   Stg_Component_Build( vc, 0, False );
+   /* Create CompositeVC */
+   cvc = CompositeVC_New( "CompositeVC", variable_Register, conFunc_Register, dictionary, mesh );
 
-   for (j = 0; j < 6; j++)
+	for (i = 0; i < 6; i++) {
+		vc = WallVC_New( vcKeyName[i], vcKey[i], variable_Register, conFunc_Register, dictionary, mesh );
+		Stg_Component_Build( vc, 0, False );
+		CompositeVC_Add(cvc, vc, True);
+	}
+
+	Stg_Component_Build( cvc, 0, False );
+
+	for (j = 0; j < 6; j++)
       memset(array[j], 0, sizeof(double)*nDomains);
-   memset(array[6], 0, sizeof(double)*nDomains*5);
-   VariableCondition_Apply(vc, NULL);
+	memset(array[6], 0, sizeof(double)*nDomains*5);
+	VariableCondition_Apply(cvc, NULL);
 
 	if (data->rank == procToWatch) {
-		for (j = 0; j < 6; j++) {
-			Journal_Printf( stream, "\nvar[%u]: %.2lf", j, array[j][0]) ;
+      for (j = 0; j < 6; j++) {
+			Journal_Printf( stream, "\nvar[%u]: %.2lf", j, array[j][0]);
 			for (k = 1; k < nDomains; k++)
-				Journal_Printf( stream, ", %.2lf", array[j][k] );
-      }
-		Journal_Printf( stream, "\nvar[6]: %.2lf", array[6][0] );
+				Journal_Printf( stream, ", %.2lf", array[j][k]);
+		} Journal_Printf( stream, "\nvar[7]: %.2lf", array[6][0]);
+
 		for (j = 1; j < nDomains*5; j++)
-			Journal_Printf( stream, ", %.2lf", array[6][j] );
-		Journal_Printf( stream, "\n\n" );
+			Journal_Printf( stream, ", %.2lf", array[6][j]);
+		Journal_Printf( stream, "\n\n");
 
 		for (j = 0; j < 7; j++) {
 			for (k = 0; k < nDomains; k++)
-				Journal_Printf( stream, "%s ", VariableCondition_IsCondition(vc, k, j) ? "True " : "False" );
-			Journal_Printf( stream, "\n" );
-		} Journal_Printf( stream, "\n" );
+				Journal_Printf( stream, "%s ", VariableCondition_IsCondition(cvc, k, j) ? "True " : "False");
+			Journal_Printf( stream, "\n");
+		} Journal_Printf( stream, "\n");
 
 		for (j = 0; j < 7; j++) {
 			for (k = 0; k < nDomains; k++) {
 				VariableCondition_ValueIndex  valIndex;
 
-            valIndex = VariableCondition_GetValueIndex(vc, k, j);
-            if (valIndex != (unsigned)-1)
+				valIndex = VariableCondition_GetValueIndex(cvc, k, j);
+				if (valIndex != (unsigned)-1)
 					Journal_Printf( stream, "%03u ", valIndex);
-            else
+				else
 					Journal_Printf( stream, "XXX ");
-			} Journal_Printf( stream, "\n" );
-		} Journal_Printf( stream, "\n" );
+			} Journal_Printf( stream, "\n");
+		} Journal_Printf( stream, "\n");
 
-		pcu_filename_expected( "testAllNodesVC.expected", expected_file );
-		pcu_check_fileEq( "testAllNodesVC.dat", expected_file );
-		remove( "testAllNodesVC.dat" );
-	}
-
-   Stg_Class_Delete(vc);
-   Stg_Class_Delete(variable_Register);
-
-   for (i = 0; i < 7; i++) {
-      Stg_Class_Delete(var[i]);
-      if (array[i]) Memory_Free(array[i]);
+		pcu_filename_expected( "testCompositeVC.expected", expected_file );
+		pcu_check_fileEq( "testCompositeVC.dat", expected_file );
+		remove( "testCompositeVC.dat" );
    }
 
-   Stg_Class_Delete(conFunc_Register);
-   Stg_Class_Delete(dictionary);
-   FreeObject( mesh );
+	Stg_Class_Delete(cvc);
+	Stg_Class_Delete(variable_Register);
+	for (i = 0; i < 7; i++) {
+		Stg_Class_Delete(var[i]);
+		if (array[i]) Memory_Free(array[i]);
+	}
+	Stg_Class_Delete(conFunc_Register);
+	Stg_Class_Delete(quadCF);
+	Stg_Class_Delete(expCF);
+	Stg_Class_Delete(dictionary);
+	FreeObject( mesh );
 }
 
-
-void AllNodesVCSuite( pcu_suite_t* suite ) {
-	pcu_suite_setData( suite, AllNodesVCSuiteData );
-   pcu_suite_setFixtures( suite, AllNodesVCSuite_Setup, AllNodesVCSuite_Teardown );
-   pcu_suite_addTest( suite, AllNodesVCSuite_TestAllNodesVC );
+void CompositeVCSuite( pcu_suite_t* suite ) {
+	pcu_suite_setData( suite, CompositeVCSuiteData );
+   pcu_suite_setFixtures( suite, CompositeVCSuite_Setup, CompositeVCSuite_Teardown );
+   pcu_suite_addTest( suite, CompositeVCSuite_TestCompositeVC );
 }
---
+++ Utils/tests/CompositeVCSuite.h
@@ -0,0 +1,6 @@
+#ifndef StGermain_CompositeVCSuite_h
+#define StGermain_CompositeVCSuite_h
+
+void CompositeVCSuite( pcu_suite_t* suite );
+
+#endif
