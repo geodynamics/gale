f60b6641a681b76fb1dc1c67d58dfc43fc65721a
JericoRevote <devnull@localhost>
2009-09-01 12:28:23 +1000

    Updated TrigMathSuite to divide each test cases into separate functions.

--- Geometry/tests/TrigMathSuite.c
+++ Geometry/tests/TrigMathSuite.c
@@ -45,93 +45,120 @@
 #include "TrigMathSuite.h"
 
 typedef struct {
-	MPI_Comm			comm;
-	unsigned int	rank;
-	unsigned int	nProcs;
+	MPI_Comm	comm;
+	unsigned rank;
+	unsigned nProcs;
+	double	angle;
+	double	rectOriginal[3];
+	double	spherical[3];
+	double	rectangular[3];
+	Index		dim;
 } TrigMathSuiteData;
+
+void TrigMathSuite_DomainFindingFunction( TrigMathSuiteData* data, double angle ) {
+	char base[24];
+	char stg[24];	
+
+	data->angle = angle;
+	sprintf( base, "%lf", sin(data->angle) );
+	sprintf( stg, "%lf", sin( StGermain_TrigDomain(data->angle) ) );
+	pcu_check_streq( base, stg );
+}
 
 void TrigMathSuite_Setup( TrigMathSuiteData* data ) {
 	/* MPI Initializations */
 	data->comm = MPI_COMM_WORLD;
 	MPI_Comm_rank( data->comm, &data->rank );
 	MPI_Comm_size( data->comm, &data->nProcs );
+
+	data->rectOriginal[0] = 2.4; data->rectOriginal[1] = 5; data->rectOriginal[2] = -10;
 }
 
 void TrigMathSuite_Teardown( TrigMathSuiteData* data ) {
 }
 
-void TrigMathSuite_TestTrigMath( TrigMathSuiteData* data ) {
-	unsigned	procToWatch;
-	procToWatch = data->nProcs >=2 ? 1 : 0;
+void TrigMathSuite_TestAngleConversionMacros( TrigMathSuiteData* data ) {
+	char buffer[24];
 	
-	if (data->rank == procToWatch) {
-		double	angle;
-		double	rectOriginal[] = {2.4,5,-10};
-		double	spherical[3];
-		double	rectangular[3];
-		Index		dim;
-		char		expected_file[PCU_PATH_MAX];
-		Stream*	stream = Journal_Register( Info_Type, "TrigMathStream" );
-		Stream_RedirectFile( stream, "testTrigMath.dat" );
+	sprintf( buffer, "%2.3f", StGermain_DegreeToRadian( 37.0 + 3 ));
+	pcu_check_streq( buffer, "0.698" );
+	sprintf( buffer, "%2.3f", StGermain_RadianToDegree( 1.234 * 2 ));
+	pcu_check_streq( buffer, "141.406" );
+}
 
-		Journal_Printf( stream, "****************************\n");
-		Journal_Printf( stream, "Test angle conversion macros\n");
-		Journal_Printf( stream, "39 degrees in radians = %2.3f\n", StGermain_DegreeToRadian( 37.0 + 3 ) );
-		Journal_Printf( stream, "2.468 radians in degrees = %2.3f\n", StGermain_RadianToDegree( 1.234 * 2 ) );
+void TrigMathSuite_TestDomainFindingFunction( TrigMathSuiteData* data ) {
+	TrigMathSuite_DomainFindingFunction( data, 1.5 );
+	TrigMathSuite_DomainFindingFunction( data, -1.2 );
+	TrigMathSuite_DomainFindingFunction( data, 20.0 );
+}
 
-		Journal_Printf( stream, "\n****************************\n");
-		Journal_Printf( stream, "Test domain finding function\n");
-		angle = 1.5;
-		Journal_Printf( stream, "Angle %lf is equivalent to %lf\n", angle, StGermain_TrigDomain(angle) );
-		Journal_Printf( stream, "Sine test: %lf = %lf\n", sin(angle), sin( StGermain_TrigDomain(angle) ) );
-		angle = -1.2;
-		Journal_Printf( stream, "Angle %lf is equivalent to %lf\n", angle, StGermain_TrigDomain(angle) );
-		Journal_Printf( stream, "Sine test: %lf = %lf\n", sin(angle), sin( StGermain_TrigDomain(angle) ) );
-		angle = 20.0;
-		Journal_Printf( stream, "Angle %lf is equivalent to %lf\n", angle, StGermain_TrigDomain(angle) );
-		Journal_Printf( stream, "Sine test: %lf = %lf\n", sin(angle), sin( StGermain_TrigDomain(angle) ) );
+void TrigMathSuite_TestQuadrantFindingFunction( TrigMathSuiteData* data ) {
+	data->angle = 45;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 0 );
+	data->angle = 120;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 1 );
+	data->angle = 195;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 2 );
+	data->angle = 340;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 3 );
+	data->angle = 730;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 0 );
+	data->angle = -135;
+	pcu_check_true( StGermain_TrigQuadrant( StGermain_DegreeToRadian(data->angle) ) == 2 );
+}
 
-		Journal_Printf( stream, "\n****************************\n");
-		Journal_Printf( stream, "Test quadrant finding function\n");
-		angle = 45;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
-		angle = 120;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
-		angle = 195;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
-		angle = 340;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
-		angle = 730;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
-		angle = -135;
-		Journal_Printf( stream, "Angle %lf degrees is in quadrant %d\n", angle, StGermain_TrigQuadrant( StGermain_DegreeToRadian(angle) ));
+void TrigMathSuite_TestCoordinateConversionFunction2D( TrigMathSuiteData* data ) {
+	char x[24], y[24];
 
-		Journal_Printf( stream, "\n****************************\n");
-		Journal_Printf( stream, "Test coordinate conversion functions 2D\n");
-		dim = 2;
-		StGermain_PrintNamedVector( stream, rectOriginal, dim );
-		StGermain_RectangularToSpherical( spherical, rectOriginal, dim );
-		StGermain_PrintNamedVector( stream, spherical, dim );
-		StGermain_SphericalToRectangular( rectangular, spherical, dim );
-		StGermain_PrintNamedVector( stream, rectangular, dim );
+	data->dim = 2;
+	sprintf( x, "%lf", data->rectOriginal[0] );
+	sprintf( y, "%lf", data->rectOriginal[1] );
+	pcu_check_streq( x, "2.400000" );
+	pcu_check_streq( y, "5.000000" );
+	StGermain_RectangularToSpherical( data->spherical, data->rectOriginal, data->dim );
+	sprintf( x, "%lf", data->spherical[0] );
+	sprintf( y, "%lf", data->spherical[1] );
+	pcu_check_streq( x, "5.546170" );
+	pcu_check_streq( y, "1.123276" );
+	StGermain_SphericalToRectangular( data->rectangular, data->spherical,data-> dim );
+	sprintf( x, "%lf", data->rectangular[0] );
+	sprintf( y, "%lf", data->rectangular[1] );
+	pcu_check_streq( x, "2.400000" );
+	pcu_check_streq( y, "5.000000" );
+}
 
-		Journal_Printf( stream, "\n****************************\n");
-		Journal_Printf( stream, "Test coordinate conversion functions 3D\n");
-		dim = 3;
-		StGermain_PrintNamedVector( stream, rectOriginal, dim );
-		StGermain_RectangularToSpherical( spherical, rectOriginal, dim );
-		StGermain_PrintNamedVector( stream, spherical, dim );
-		StGermain_SphericalToRectangular( rectangular, spherical, dim );
-		StGermain_PrintNamedVector( stream, rectangular, dim );
+void TrigMathSuite_TestCoordinateConversionFunction3D( TrigMathSuiteData* data ) {
+	char x[24], y[24], z[24];
 
-		pcu_filename_expected( "testTrigMath.expected", expected_file );
-		pcu_check_fileEq( "testTrigMath.dat", expected_file );
-		remove( "testTrigMath.dat" );
-	}
+	data->dim = 3;
+	sprintf( x, "%lf", data->rectOriginal[0] );
+	sprintf( y, "%lf", data->rectOriginal[1] );
+	sprintf( z, "%lf", data->rectOriginal[2] );
+	pcu_check_streq( x, "2.400000" );
+	pcu_check_streq( y, "5.000000" );
+	pcu_check_streq( z, "-10.000000" );
+	StGermain_RectangularToSpherical( data->spherical, data->rectOriginal, data->dim );
+	sprintf( x, "%lf", data->spherical[0] );
+	sprintf( y, "%lf", data->spherical[1] );
+	sprintf( z, "%lf", data->spherical[2] );
+	pcu_check_streq( x, "11.435034" );
+	pcu_check_streq( y, "1.123276" );
+	pcu_check_streq( z, "2.635212" );
+	StGermain_SphericalToRectangular( data->rectangular, data->spherical,data-> dim );
+	sprintf( x, "%lf", data->rectangular[0] );
+	sprintf( y, "%lf", data->rectangular[1] );
+	sprintf( z, "%lf", data->rectangular[2] );
+	pcu_check_streq( x, "2.400000" );
+	pcu_check_streq( y, "5.000000" );
+	pcu_check_streq( z, "-10.000000" );
 }
 
 void TrigMathSuite( pcu_suite_t* suite ) {
 	pcu_suite_setData( suite, TrigMathSuiteData );
    pcu_suite_setFixtures( suite, TrigMathSuite_Setup, TrigMathSuite_Teardown );
-   pcu_suite_addTest( suite, TrigMathSuite_TestTrigMath );
+   pcu_suite_addTest( suite, TrigMathSuite_TestAngleConversionMacros );
+   pcu_suite_addTest( suite, TrigMathSuite_TestDomainFindingFunction );
+   pcu_suite_addTest( suite, TrigMathSuite_TestQuadrantFindingFunction );
+   pcu_suite_addTest( suite, TrigMathSuite_TestCoordinateConversionFunction2D );
+   pcu_suite_addTest( suite, TrigMathSuite_TestCoordinateConversionFunction3D );
 }
