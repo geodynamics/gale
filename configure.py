6397e996ee0a38e4b1cc2aabc7259b9341d05f8d
luke <luke@the-spatula>
2009-10-01 12:51:45 +1000

    Updating stgUnderworld to reflect changes in stgUnderworldE. Mostly moving the scons subdirectory into config.

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys, subprocess, shutil, os
 subp = subprocess.Popen(
-    'scons/scons.py --config=force -f SConfigure ' + ' '.join(sys.argv[1:]), shell=True
+    'config/scons/scons.py --config=force -f SConfigure ' + ' '.join(sys.argv[1:]), shell=True
 )
 subp.wait()
--- scons.py
+++ scons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys, subprocess
 subp = subprocess.Popen(
-    'scons/scons.py ' + ' '.join(sys.argv[1:]), shell=True
+    'config/scons/scons.py ' + ' '.join(sys.argv[1:]), shell=True
 )
 subp.wait()
