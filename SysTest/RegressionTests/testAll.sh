cda0e36cd68ed1a8872d2c111cfebcdc58618814
luke <luke@the-spatula>
2009-10-13 11:35:59 +1100

    Converted the cosine-hill dual mesh test.

--- SysTest/RegressionTests/testAll.sh
+++ SysTest/RegressionTests/testAll.sh
@@ -1,3 +1,6 @@
 ./checkpointTest-noStokes.pl CosineHillRotateBC.xml -optionsFile np-1.dat
 ./checkpointTest-noStokes.pl CosineHillRotateBC.xml -optionsFile np-2.dat
 ./checkpointTest-noStokes.pl CosineHillRotateBC.xml -optionsFile np-4.dat
+./checkpointTest-noStokes.pl CosineHillRotateBC-DualMesh.xml -optionsFile np-1.dat
+./checkpointTest-noStokes.pl CosineHillRotateBC-DualMesh.xml -optionsFile np-2.dat
+./checkpointTest-noStokes.pl CosineHillRotateBC-DualMesh.xml -optionsFile np-4.dat
