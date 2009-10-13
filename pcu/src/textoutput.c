66d6ff400e23a0b4f1e3b8cd0c324e15a3766623
JericoRevote <devnull@localhost>
2009-10-13 13:01:59 +1100

    Just putting a line feed on the textoutput function, makes it easier to view the dump log output file.

--- pcu/src/textoutput.c
+++ pcu/src/textoutput.c
@@ -81,8 +81,7 @@ void printstatus( pcu_listener_t* lsnr, pcu_suite_t* suite, int final ) {
       return;
 
    printf( "Running suite '%s', passes: %d/%d%s", 
-	   suite->name, suite->npassed, suite->ntests, 
-	   final ? "\n" : "\r" );
+	   suite->name, suite->npassed, suite->ntests, "\n" );
 }
 
 void printsources( pcu_listener_t* lsnr, pcu_suite_t* suite ) {
