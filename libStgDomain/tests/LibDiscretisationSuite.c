c60644e55929514a17bf7a36e9de532e0b498fef
JericoRevote <devnull@localhost>
2009-10-16 17:51:45 +1100

    Adding LibDiscretisationSuite pcu test.

---
+++ libStgDomain/tests/LibDiscretisationSuite.c
@@ -0,0 +1,45 @@
+#include <mpi.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pcu/pcu.h"
+#include <StGermain/StGermain.h>
+#include "StgDomain/StgDomain.h"
+
+#include "LibDiscretisationSuite.h"
+
+typedef struct {
+} LibDiscretisationSuiteData;
+
+void LibDiscretisationSuite_Setup( LibDiscretisationSuiteData* data ) {
+}
+
+void LibDiscretisationSuite_Teardown( LibDiscretisationSuiteData* data ) {
+}
+
+void LibDiscretisationSuite_DirectoryStGermain( LibDiscretisationSuiteData* data ) {
+	Stg_Object* testDirectoryStGermain;
+
+	testDirectoryStGermain = Stg_ObjectList_Get( Project_XMLSearchPaths, "StGermain" );
+	pcu_check_true( testDirectoryStGermain != NULL );
+}
+
+void LibDiscretisationSuite_DirectoryDiscretisation( LibDiscretisationSuiteData * data ) {
+	Stg_Object* testDirectoryStGermain;
+	Stg_Object* testDirectoryDiscretisation;
+
+	testDirectoryStGermain = Stg_ObjectList_Get( Project_XMLSearchPaths, "StGermain" );
+	testDirectoryDiscretisation = Stg_ObjectList_Get( Project_XMLSearchPaths, "StgDomain" );
+
+	pcu_check_true( ( strcmp((char*)LIB_DIR, (char*)testDirectoryStGermain) ) || ( testDirectoryDiscretisation != NULL ) );
+}
+
+void LibDiscretisationSuite( pcu_suite_t* suite ) {
+	pcu_suite_setData( suite, LibDiscretisationSuiteData );
+	pcu_suite_setFixtures( suite, LibDiscretisationSuite_Setup, LibDiscretisationSuite_Teardown);
+
+	pcu_suite_addTest( suite, LibDiscretisationSuite_DirectoryStGermain );
+	pcu_suite_addTest( suite, LibDiscretisationSuite_DirectoryDiscretisation );
+}
