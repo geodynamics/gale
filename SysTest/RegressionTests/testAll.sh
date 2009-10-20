abe5029994bf0cfedc9ef657e83b7cb11070b653
luke <luke@the-spatula>
2009-10-20 11:04:05 +1100

    Adding the analytic shear test to PICellerator.

---
+++ SysTest/RegressionTests/testAll.sh
@@ -0,0 +1,4 @@
+./analyticTest.pl AnalyticShearXZ.xml -optionsFile np-1.dat
+./analyticTest.pl AnalyticShearXZ.xml -optionsFile np-2.dat
+./analyticTest.pl AnalyticShearXZ.xml -optionsFile np-4.dat
+./analyticTest.pl AnalyticShearXZ.xml -optionsFile np-8.dat
