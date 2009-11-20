d85b8d251ae21c3e8000a7611f9aaf44235866c2
JericoRevote <devnull@localhost>
2009-11-20 15:52:43 +1100

    Rejiged this SwarmVariable child.

--- Discretisation/src/FeSwarmVariable.c
+++ Discretisation/src/FeSwarmVariable.c
@@ -53,51 +53,40 @@
 
 const Type FeSwarmVariable_Type = "FeSwarmVariable";
 
-FeSwarmVariable* _FeSwarmVariable_New(
- 		SizeT                                              _sizeOfSelf, 
-		Type                                               type,
-		Stg_Class_DeleteFunction*                          _delete,
-		Stg_Class_PrintFunction*                           _print, 
-		Stg_Class_CopyFunction*                            _copy, 
-		Stg_Component_DefaultConstructorFunction*          _defaultConstructor,
-		Stg_Component_ConstructFunction*                   _construct,
-		Stg_Component_BuildFunction*                       _build,
-		Stg_Component_InitialiseFunction*                  _initialise,
-		Stg_Component_ExecuteFunction*                     _execute,
-		Stg_Component_DestroyFunction*                     _destroy,
-		SwarmVariable_ValueAtFunction*                     _valueAt,
-		SwarmVariable_GetGlobalValueFunction*              _getMinGlobalMagnitude,
-		SwarmVariable_GetGlobalValueFunction*              _getMaxGlobalMagnitude,
-		Name                                               name )
-{
-	FeSwarmVariable*		    self;
+void* _FeSwarmVariable_DefaultNew( Name name ) {
+	return _FeSwarmVariable_New( 
+		sizeof(FeSwarmVariable), 
+		FeSwarmVariable_Type, 
+		_FeSwarmVariable_Delete, 
+		_FeSwarmVariable_Print,
+		_FeSwarmVariable_Copy, 
+		_FeSwarmVariable_DefaultNew,
+		_FeSwarmVariable_AssignFromXML,
+		_FeSwarmVariable_Build, 
+		_FeSwarmVariable_Initialise, 
+		_FeSwarmVariable_Execute,
+		_FeSwarmVariable_Destroy,
+		name,
+		NON_GLOBAL,
+		_FeSwarmVariable_ValueAt,
+		_FeSwarmVariable_GetMinGlobalMagnitude,
+		_FeSwarmVariable_GetMaxGlobalMagnitude );
+}
+
+FeSwarmVariable* _FeSwarmVariable_New( FESWARMVARIABLE_DEFARGS ) {
+	FeSwarmVariable* self;
 	
 	/* Allocate memory */
-	assert( _sizeOfSelf >= sizeof(FeSwarmVariable) );
-	self = (FeSwarmVariable*) _SwarmVariable_New( 
-			_sizeOfSelf, 
-			type, 
-			_delete,
-			_print, 
-			_copy,
-			_defaultConstructor,
-			_construct,
-			_build, 
-			_initialise, 
-			_execute,
-			_destroy,
-			_valueAt,
-			_getMinGlobalMagnitude,
-			_getMaxGlobalMagnitude,
-			name );
+	assert( sizeOfSelf >= sizeof(FeSwarmVariable) );
+	self = (FeSwarmVariable*) _SwarmVariable_New( SWARMVARIABLE_PASSARGS );
 
 	return self;
 }
 
 void _FeSwarmVariable_Init( void* swarmVariable, FeVariable* feVariable ) {
-	FeSwarmVariable*         self              = (FeSwarmVariable*) swarmVariable;
+	FeSwarmVariable* self = (FeSwarmVariable*) swarmVariable;
 
-	self->feVariable                = feVariable;
+	self->feVariable = feVariable;
    /* variable does not store data, so is not checkpointed */
    self->isCheckpointedAndReloaded = False;
 }
@@ -116,45 +105,23 @@ void _FeSwarmVariable_Print( void* _swarmVariable, Stream* stream ) {
 	Journal_PrintPointer( stream, self->feVariable );
 }
 
-
 void* _FeSwarmVariable_Copy( void* swarmVariable, void* dest, Bool deep, Name nameExt, PtrMap* ptrMap ) {
 	FeSwarmVariable*	self = (FeSwarmVariable*)swarmVariable;
 	FeSwarmVariable*	newFeSwarmVariable;
 	
 	newFeSwarmVariable = _SwarmVariable_Copy( self, dest, deep, nameExt, ptrMap );
 	
-	newFeSwarmVariable->feVariable              = self->feVariable;
+	newFeSwarmVariable->feVariable = self->feVariable;
 	
 	return (void*)newFeSwarmVariable;
 }
 
-void* _FeSwarmVariable_DefaultNew( Name name ) {
-		return _FeSwarmVariable_New( 
-			sizeof(FeSwarmVariable), 
-			FeSwarmVariable_Type, 
-			_FeSwarmVariable_Delete, 
-			_FeSwarmVariable_Print,
-			_FeSwarmVariable_Copy, 
-			_FeSwarmVariable_DefaultNew,
-			_FeSwarmVariable_AssignFromXML,
-			_FeSwarmVariable_Build, 
-			_FeSwarmVariable_Initialise, 
-			_FeSwarmVariable_Execute,
-			_FeSwarmVariable_Destroy,
-			_FeSwarmVariable_ValueAt,
-			_FeSwarmVariable_GetMinGlobalMagnitude,
-			_FeSwarmVariable_GetMaxGlobalMagnitude,
-			name );
-}
-
 void _FeSwarmVariable_AssignFromXML( void* swarmVariable, Stg_ComponentFactory* cf, void* data ) {
-	FeSwarmVariable*        self       = (FeSwarmVariable*) swarmVariable;
+	FeSwarmVariable* self = (FeSwarmVariable*) swarmVariable;
 
 	_SwarmVariable_AssignFromXML( self, cf, data );
 
-	_FeSwarmVariable_Init( 
-		self, 
-		Stg_ComponentFactory_ConstructByKey( cf, self->name, "FeVariable", FeVariable, True, data ) ) ;
+	_FeSwarmVariable_Init( self, Stg_ComponentFactory_ConstructByKey( cf, self->name, "FeVariable", FeVariable, True, data ) );
 }
 
 void _FeSwarmVariable_Build( void* swarmVariable, void* data ) {
@@ -170,20 +137,25 @@ void _FeSwarmVariable_Initialise( void* swarmVariable, void* data ) {
 }
 
 void _FeSwarmVariable_Execute( void* swarmVariable, void* data ) {}
-void _FeSwarmVariable_Destroy( void* swarmVariable, void* data ) {}
+
+void _FeSwarmVariable_Destroy( void* swarmVariable, void* data ) {
+	FeSwarmVariable* self = (FeSwarmVariable*) swarmVariable;
+
+	_SwarmVariable_Destroy( self, data );
+}
 
 void _FeSwarmVariable_ValueAt( void* swarmVariable, Particle_Index lParticle_I, double* value ) {
-	FeSwarmVariable*  self            = (FeSwarmVariable*) swarmVariable;
-	FeMesh*           mesh            = self->feVariable->feMesh;
-	Swarm*            swarm           = self->swarm;
-	GlobalParticle*   particle        = (GlobalParticle*) Swarm_ParticleAt( swarm, lParticle_I );
+	FeSwarmVariable*  self = (FeSwarmVariable*) swarmVariable;
+	FeMesh*           mesh = self->feVariable->feMesh;
+	Swarm*            swarm = self->swarm;
+	GlobalParticle*   particle = (GlobalParticle*) Swarm_ParticleAt( swarm, lParticle_I );
 	double            xi[3];
 
 	/* check if the swarm is using a GlobalCoordSystem, if not FAIL */
 	Journal_Firewall( (swarm->particleLayout->coordSystem == GlobalCoordSystem), 
-			Journal_Register( Error_Type, "FeSwarmVariable" ),
+		Journal_Register( Error_Type, "FeSwarmVariable" ),
 			"Error in function %s: FeSwarmVariables require swarms with GlobalCoordSystems\n"
-		        "This FeSwarmVariable, %s, uses the swarm %s, which doesn't have a GlobalCoordSystem\n",
+			"This FeSwarmVariable, %s, uses the swarm %s, which doesn't have a GlobalCoordSystem\n",
 			__func__, self->name, swarm->name );
 	FeMesh_CoordGlobalToLocal( mesh, particle->owningCell, particle->coord, xi );
 	FeVariable_InterpolateWithinElement( self->feVariable, particle->owningCell, xi, value );
@@ -191,12 +163,12 @@ void _FeSwarmVariable_ValueAt( void* swarmVariable, Particle_Index lParticle_I, 
 
 
 double _FeSwarmVariable_GetMinGlobalMagnitude( void* swarmVariable ) {
-	FeSwarmVariable*  self            = (FeSwarmVariable*) swarmVariable;
+	FeSwarmVariable*  self = (FeSwarmVariable*) swarmVariable;
 
 	return FieldVariable_GetMinGlobalFieldMagnitude( self->feVariable );
 }
 double _FeSwarmVariable_GetMaxGlobalMagnitude( void* swarmVariable ) {
-	FeSwarmVariable*  self            = (FeSwarmVariable*) swarmVariable;
+	FeSwarmVariable*  self = (FeSwarmVariable*) swarmVariable;
 
 	return FieldVariable_GetMaxGlobalFieldMagnitude( self->feVariable );
 }
