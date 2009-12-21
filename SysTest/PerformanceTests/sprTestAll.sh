698b88b50496eb04725432e82e73b5a5ab07f8a9
JulianGiordani <devnull@localhost>
2009-12-21 17:25:34 +1100

    Adding convergence tests for spr

---
+++ SysTest/PerformanceTests/sprTestAll.sh
@@ -0,0 +1,5 @@
+./runAndTestConvergence.pl testVelicSolA.xml -optionsFile OFileSPR.dat
+./runAndTestConvergence.pl testVelicSolB.xml -optionsFile OFileSPR.dat
+./runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFileSPR.dat
+./runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFileSPR.dat
+./runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFileSPR.dat 
