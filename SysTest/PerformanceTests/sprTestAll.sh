36df22ae90b19493fcc3b4105eb697762c2244e2
JulianGiordani <devnull@localhost>
2009-12-22 12:46:54 +1100

    more systesting converting to python. Also removing some old python scripts that no one was using

--- SysTest/PerformanceTests/sprTestAll.sh
+++
@@ -1,5 +0,0 @@
-./runAndTestConvergence.pl testVelicSolA.xml -optionsFile OFileSPR.dat
-./runAndTestConvergence.pl testVelicSolB.xml -optionsFile OFileSPR.dat
-./runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFileSPR.dat
-./runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFileSPR.dat
-./runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFileSPR.dat 
--- SysTest/PerformanceTests/testAll.sh
+++
@@ -1,6 +0,0 @@
-./runAndTestConvergence.pl testVelicSolA.xml -optionsFile OFile2D.dat
-./runAndTestConvergence.pl testVelicSolB.xml -optionsFile OFile2D.dat
-./runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFile2D.dat
-./runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFile2D.dat
-./runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFile2D.dat
-./runAndTestConvergence.pl testDepthDependentViscosity3D_Exponential.xml -optionsFile OFile3D.dat 
