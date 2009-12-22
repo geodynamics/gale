a1bcb9e58db15b2ebcb180eb143550312785e0ed
JulianGiordani <devnull@localhost>
2009-12-22 11:36:37 +1100

    adding the REP tests to Underworld

---
+++ SysTest/PerformanceTests/repTestAll.sh
@@ -0,0 +1,5 @@
+./runAndTestConvergence.pl testVelicSolA.xml -optionsFile OFileREP.dat
+./runAndTestConvergence.pl testVelicSolB.xml -optionsFile OFileREP.dat
+./runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFileREP.dat
+./runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFileREP.dat
+./runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFileREP.dat 
