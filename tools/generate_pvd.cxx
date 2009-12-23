e9a6a84533301d0d84c68a97e2c53d695e0911df
Walter Landry <wlandry@caltech.edu>
2009-12-23 15:25:48 -0800

    Add license for generate_pvd

--- tools/generate_pvd.cxx
+++ tools/generate_pvd.cxx
@@ -19,7 +19,7 @@
 
 **  Copyright (C) 2009, California Institute of Technology
 
-**  This script is free software; you can redistribute it and/or
+**  This software is free software; you can redistribute it and/or
 **  modify it under the terms of the GNU General Public License as
 **  published by the Free Software Foundation; either version 2 of the
 **  License, or (at your option) any later version.
