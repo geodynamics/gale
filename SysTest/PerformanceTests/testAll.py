5162ec36ec3267eee912d7a615dac029f96aa698
JericoRevote <devnull@localhost>
2010-01-06 12:56:03 +1100

    Added a method to print out the total passes/failures in the testAll* python scripts for later reporting.

--- SysTest/PerformanceTests/testAll.py
+++ SysTest/PerformanceTests/testAll.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import os
+import sys
+import subprocess
 
 def runTests():
 
@@ -9,10 +11,41 @@ def runTests():
                 './runAndTestConvergence.pl testVelicSolCx.xml -optionsFile OFile2D.dat', \
                 './runAndTestConvergence.pl testVelicSolKz.xml -optionsFile OFile2D.dat', \
                 './runAndTestConvergence.pl testVelicSolS.xml -optionsFile OFile2D.dat', \
-                './runAndTestConvergence.pl testDepthDependentViscosity3D_Exponential.xml -optionsFile OFile3D.dat' ]
+                './runAndTestConvergence.pl testDepthDependentViscosity3D_Exponential.xml -optionsFile OFile3D.dat', \
+                './sprTestAll.py', \
+                './repTestAll.py' ]
+    failed_commands = []
+    passed = 0
+    failed = 0
 
     for command in commands:
-        os.system( command )
+        try:
+            retcode = subprocess.call( command, shell=True )
+            if retcode == 0:
+                 passed += 1
+            else:
+                 failed += 1
+                 failed_commands.append( command )
+        except OSError, e:
+            print >>sys.stderr, "Execution Failed:", e
 
+    filename = "../../../summary.dat"
+
+    if os.path.exists( filename ):
+        FILE = open( filename, "a" )
+    else:
+        FILE = open( filename, "w" )
+
+    message = ''
+    message += "--------------------------------------------------------\n" + \
+          "[SYS] Underworld Convergence Tests:\n" + \
+          "[SYS]      Total Passes: (" + str(passed) + "/" + str(len( commands )) + ")\n" \
+          "[SYS]      Failed Commands:\n"
+    for command in failed_commands:
+        message += "[SYS]            " + command + "\n"
+    message += "--------------------------------------------------------\n"
+    FILE.write( message )
+    print message
+    FILE.close()
 
 runTests()
