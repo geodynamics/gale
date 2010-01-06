b7ecb11b3a6ab5adbb54723670d995afab9e4a60
JericoRevote <devnull@localhost>
2010-01-06 12:49:26 +1100

    Added a method to print out the total passes/failures in the testAll* python scripts for later reporting.

--- SysTest/PerformanceTests/testAll.sh
+++
@@ -1,3 +0,0 @@
-./runAndTestConvergence.pl AnalyticSinusoid.xml -optionsFile as-np-1.dat
-./runAndTestConvergence.pl AnalyticSinusoid.xml -optionsFile as-np-2.dat
-./runAndTestConvergence.pl AnalyticSinusoid.xml -optionsFile as-np-4.dat
--- SysTest/RegressionTests/testAll.py
+++ SysTest/RegressionTests/testAll.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import os
+import sys
+import subprocess
 
 def runTests():
 
@@ -36,8 +38,38 @@ def runTests():
                 './analyticTest.pl TempDiffusion.xml -optionsFile np-4.dat', \
                 './analyticTest.pl TempDiffusion.xml -optionsFile np-8.dat', \
                 './checkpointTest.pl Multigrid.xml']
+    failed_commands = []
+    passed = 0
+    failed = 0
 
     for command in commands:
-        os.system( command )
+        try:
+            retcode = subprocess.call( command, shell=True )
+            if retcode == 0:
+                 passed += 1
+            else:
+                 failed += 1
+                 failed_commands.append( command )
+        except OSError, e:
+            print >>sys.stderr, "Execution Failed:", e
+
+    filename = "../../../summary.dat"
+
+    if os.path.exists( filename ):
+        FILE = open( filename, "a" )
+    else:
+        FILE = open( filename, "w" )
+
+    message = ''
+    message += "--------------------------------------------------------\n" + \
+          "[SYS] StgFEM Normal-Res Integration Tests:\n" + \
+          "[SYS]      Total Passes: (" + str(passed) + "/" + str(len( commands )) + ")\n" \
+          "[SYS]      Failed Commands:\n"
+    for command in failed_commands:
+        message += "[SYS]            " + command + "\n"
+    message += "--------------------------------------------------------\n"
+    FILE.write( message )
+    print message
+    FILE.close()
 
 runTests()
