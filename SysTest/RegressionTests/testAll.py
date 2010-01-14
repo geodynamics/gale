918f0d0658b5dab293ee0792fe4a4d8f091653de
JericoRevote <devnull@localhost>
2010-01-14 17:40:20 +1100

    Updated testAll.py script to include results summary.

--- SysTest/RegressionTests/testAll.py
+++ SysTest/RegressionTests/testAll.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import os
+import sys
+import subprocess
 
 def runTests():
     
@@ -9,7 +11,39 @@ def runTests():
                 './analyticTest.pl AnalyticShearXZ.xml -optionsFile np-4.dat', \
                 './analyticTest.pl AnalyticShearXZ.xml -optionsFile np-8.dat']
 
+    failed_commands = [] 
+    passed = 0
+    failed = 0
+ 
     for command in commands:
-        os.system( command )
+        try:
+            retcode = subprocess.call( command+' -serial' , shell=True )
+            if retcode == 0:
+                 passed += 1
+            else:
+                 failed += 1
+                 failed_commands.append( command )
+        except OSError, e:
+            print >>sys.stderr, "Execution Failed:", e
+
+    filename = "../../../summary.dat"
+
+    if os.path.exists( filename ):
+        FILE = open( filename, "a" )
+    else:
+        FILE = open( filename, "w" )
+
+    message = ''
+    message += "--------------------------------------------------------\n" + \
+          "[SYS] PICellerator Normal-Res Integration Tests:\n" + \
+          "[SYS]      Total Passes: (" + str(passed) + "/" + str(len( commands )) + ")\n" \
+          "[SYS]      Failed Commands:\n"
+    for command in failed_commands:
+        message += "[SYS]            " + command + "\n"
+    message += "--------------------------------------------------------\n"
+    FILE.write( message )
+    print message
+    FILE.close()
+
 
 runTests()
