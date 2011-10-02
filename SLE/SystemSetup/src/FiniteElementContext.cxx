48a0467effbe5fe04e3e496c65f7addf2eeff1bf
Walter Landry <wlandry@caltech.edu>
2011-10-02 11:01:01 -0700

    Make _FiniteElementContext_DumpMeshHDF5 output quad and discontinuous elements in a non-jumbled order

--- SLE/SystemSetup/src/FiniteElementContext.cxx
+++ SLE/SystemSetup/src/FiniteElementContext.cxx
@@ -798,13 +798,13 @@ void _FiniteElementContext_DumpMeshHDF5( void* context, FeMesh* mesh ) {
       
       for( node_I = 0 ; node_I < nodesPerEl ; node_I++ )
          buf_int[node_I] = Mesh_DomainToGlobal( mesh, MT_VERTEX, nodeList[node_I] );
-      if(nodesPerEl>3)
+      if((nDims == 2 && nodesPerEl==4) || (nDims == 3 && nodesPerEl==8))
         {
           /* some reordering is required to account for standard node ordering */ 
           buf_int[3] = Mesh_DomainToGlobal( mesh, MT_VERTEX, nodeList[2] );
           buf_int[2] = Mesh_DomainToGlobal( mesh, MT_VERTEX, nodeList[3] );
         }
-      if( nDims == 3 && nodesPerEl>7 ) {
+      if(nDims == 3 && nodesPerEl==8) {
          buf_int[7] = Mesh_DomainToGlobal( mesh, MT_VERTEX, nodeList[6] );
          buf_int[6] = Mesh_DomainToGlobal( mesh, MT_VERTEX, nodeList[7] );
       }
