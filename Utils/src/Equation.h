f696b165fba83f88019cc2ebeb9a3799368fb458
Walter Landry <wlandry@caltech.edu>
2011-11-25 15:22:38 -0800

    Add a missing include

--- Utils/src/Equation.h
+++ Utils/src/Equation.h
@@ -1,6 +1,7 @@
 #ifndef __StGermain_Utils_Equation_h__
 #define __StGermain_Utils_Equation_h__
 
+#include <string>
 double Equation_eval(const double *coord, DomainContext *context,
                      const std::string &equation);
 
